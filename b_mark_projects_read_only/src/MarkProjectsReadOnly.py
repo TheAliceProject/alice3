19f53248caaa9c5757324befc0a56354b7ae03cd
dennisc <dennisc@c85dc8a7-84c8-304b-b635-f907e77af1ba>
2009-01-22 19:18:13 +0000

    git-svn-id: https://alma.andrew.cmu.edu:8443/svn/alice3/trunk@64 c85dc8a7-84c8-304b-b635-f907e77af1ba

--- b_mark_projects_read_only/src/MarkProjectsReadOnly.py
+++ b_mark_projects_read_only/src/MarkProjectsReadOnly.py
@@ -2,14 +2,16 @@ import java
 import edu
 
 if edu.cmu.cs.dennisc.lang.SystemUtilities.isMac():
-	root = "/Applications/Programming/Alice"
+	pre = "/Applications/Programming/Alice/"
+	post = "/Alice.app/Contents/Resources/Java"
 else:
-	root = "/Program Files/Alice/"
+	pre = "/Program Files/Alice/"
+	post = ""
 
 version = edu.cmu.cs.dennisc.alice.Version.getCurrentVersionText()
 
-dir = java.io.File( root + version + "/application/projects" )
-
+dir = java.io.File( pre + version + post + "/application/projects" )
+print dir
 for file in edu.cmu.cs.dennisc.io.FileUtilities.listDescendants( dir, "a3p" ):
 	if file.setReadOnly():
 		print "success setReadOnly:", file 
