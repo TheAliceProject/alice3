468c581c948ebc60b8748c294b069607f12a19fb
dennisc <dennisc@c85dc8a7-84c8-304b-b635-f907e77af1ba>
2009-01-27 01:05:28 +0000

    git-svn-id: https://alma.andrew.cmu.edu:8443/svn/alice3/trunk@74 c85dc8a7-84c8-304b-b635-f907e77af1ba

--- rt_foundation/src/edu/cmu/cs/dennisc/scenegraph/Torus.java
+++ rt_foundation/src/edu/cmu/cs/dennisc/scenegraph/Torus.java
@@ -28,36 +28,62 @@ package edu.cmu.cs.dennisc.scenegraph;
  */
 public class Torus extends Shape {
 	//todo: rename
-	private enum E {
-		YES( true ), NO( true );
-		private boolean is;
-		E( boolean is ) {
-			this.is = is;
-		}
-		public double get( double yesRadius, double noRadius ) {
-			if( this.is ) {
-				return yesRadius;
-			} else {
-				return noRadius;
-			}
-		}
-	};
+//	private enum E {
+//		YES( true ), NO( true );
+//		private boolean is;
+//		E( boolean is ) {
+//			this.is = is;
+//		}
+//		public double get( double yesRadius, double noRadius ) {
+//			if( this.is ) {
+//				return yesRadius;
+//			} else {
+//				return noRadius;
+//			}
+//		}
+//	};
 	public enum CoordinatePlane {
-		XY( E.YES, E.YES, E.NO ),
-		XZ( E.YES, E.NO,  E.YES ),
-		YZ( E.NO,  E.YES, E.YES );
-		private E x;
-		private E y;
-		private E z;
-		CoordinatePlane( E x, E y, E z ) {
-			this.x = x;
-			this.y = y;
-			this.z = z;
+//		XY( E.YES, E.YES, E.NO ),
+//		XZ( E.YES, E.NO,  E.YES ),
+//		YZ( E.NO,  E.YES, E.YES );
+//		private E x;
+//		private E y;
+//		private E z;
+//		CoordinatePlane( E x, E y, E z ) {
+//			this.x = x;
+//			this.y = y;
+//			this.z = z;
+//		}
+		XY( true,  true,  false ),
+		XZ( true,  false, true ),
+		YZ( false, true,  true );
+		private boolean isX;
+		private boolean isY;
+		private boolean isZ;
+		CoordinatePlane( boolean isX, boolean isY, boolean isZ ) {
+			this.isX = isX;
+			this.isY = isY;
+			this.isZ = isZ;
 		}
 		public void updateBoundingBox( edu.cmu.cs.dennisc.math.AxisAlignedBox boundingBox, double yesRadius, double noRadius ) {
-			double x = this.x.get( yesRadius, noRadius );
-			double y = this.y.get( yesRadius, noRadius );
-			double z = this.z.get( yesRadius, noRadius );
+			double x;
+			double y;
+			double z;
+			if( this.isX ) {
+				x = yesRadius;
+			} else {
+				x = noRadius;
+			}
+			if( this.isY ) {
+				y = yesRadius;
+			} else {
+				y= noRadius;
+			}
+			if( this.isZ ) {
+				z = yesRadius;
+			} else {
+				z = noRadius;
+			}
 			boundingBox.setMinimum( -x, -y, -z );
 			boundingBox.setMaximum( +x, +y, +z );
 		}
