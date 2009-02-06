ff230a2654686aeab0ca3365ae096b85d7f1d138
dennisc <dennisc@c85dc8a7-84c8-304b-b635-f907e77af1ba>
2009-02-06 13:35:14 +0000

    git-svn-id: https://alma.andrew.cmu.edu:8443/svn/alice3/trunk@145 c85dc8a7-84c8-304b-b635-f907e77af1ba

---
+++ x_zoot/src/zoot/ZControl.java
@@ -0,0 +1,203 @@
+/*
+ * Copyright (c) 2006-2009, Carnegie Mellon University. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * 3. Products derived from the software may not be called "Alice",
+ *    nor may "Alice" appear in their name, without prior written
+ *    permission of Carnegie Mellon University.
+ * 
+ * 4. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *    "This product includes software developed by Carnegie Mellon University"
+ */
+package zoot;
+
+/**
+ * @author Dennis Cosgrove
+ */
+public abstract class ZControl extends swing.BoxPane {
+	private boolean isActive = false;
+	private boolean isPressed = false;
+	private boolean isSelected = false;
+
+	public ZControl( int axis ) {
+		super( axis );
+		if( this.isActiveMouseListeningDesired() ) {
+			this.addMouseListener( new java.awt.event.MouseListener() {
+				public void mouseEntered( java.awt.event.MouseEvent e ) {
+					if( ZControl.this.isActuallyPotentiallyActive() ) {
+//						if( getIDE().isDragInProgress() ) {
+//							//
+//						} else {
+							ZControl.this.setActive( true );
+//						}
+					}
+				}
+				public void mouseExited( java.awt.event.MouseEvent e ) {
+					if( ZControl.this.isActuallyPotentiallyActive() ) {
+//						if( getIDE().isDragInProgress() ) {
+//							//
+//						} else {
+							ZControl.this.setActive( false );
+//						}
+					}
+				}
+				public void mousePressed( java.awt.event.MouseEvent e ) {
+					if( javax.swing.SwingUtilities.isLeftMouseButton( e ) ) {
+						ZControl.this.handleLeftMousePress( e );
+					} else if( javax.swing.SwingUtilities.isRightMouseButton( e ) ) {
+						ZControl.this.handleRightMousePress( e );
+					}
+					ZControl.this.setPressed( true );
+				}
+				public void mouseReleased( java.awt.event.MouseEvent e ) {
+					ZControl.this.setPressed( false );
+				}
+				public void mouseClicked( java.awt.event.MouseEvent e ) {
+				}
+			} );
+		}
+		if( this.isSelectionMouseListeningDesired() ) { //todo: allow this value to change
+			this.addMouseListener( new java.awt.event.MouseListener() {
+				public void mouseEntered( java.awt.event.MouseEvent e ) {
+				}
+				public void mouseExited( java.awt.event.MouseEvent e ) {
+				}
+				public void mousePressed( java.awt.event.MouseEvent e ) {
+					if( javax.swing.SwingUtilities.isLeftMouseButton( e ) ) {
+						ZControl.this.handleMousePressed( e );
+					}
+				}
+				public void mouseReleased( java.awt.event.MouseEvent e ) {
+					if( javax.swing.SwingUtilities.isLeftMouseButton( e ) ) {
+						ZControl.this.handleMouseReleased( e );
+					}
+				}
+				public void mouseClicked( java.awt.event.MouseEvent e ) {
+				}
+			} );
+			this.addMouseMotionListener( new java.awt.event.MouseMotionListener() {
+				public void mouseDragged( java.awt.event.MouseEvent e ) {
+					if( javax.swing.SwingUtilities.isLeftMouseButton( e ) ) {
+						ZControl.this.handleMouseDragged( e );
+					}
+				}
+				public void mouseMoved( java.awt.event.MouseEvent e ) {
+				}
+			} );
+		}
+	}
+	protected boolean isActiveMouseListeningDesired() {
+		return isActuallyPotentiallyActive();
+	}
+	protected boolean isActuallyPotentiallyActive() {
+		return true;
+	}
+	protected void handleLeftMousePress( java.awt.event.MouseEvent e ) {
+	}
+	protected void handleRightMousePress( java.awt.event.MouseEvent e ) {
+	}
+
+	protected boolean isPressed() {
+		return this.isPressed;
+	}
+	public void setPressed( boolean isPressed ) {
+		this.isPressed = isPressed;
+	}
+
+	public boolean isActive() {
+		return this.isActive;
+	}
+	public void setActive( boolean isActive ) {
+		if( this.isActive != isActive ) {
+			this.isActive = isActive;
+			this.repaint();
+		}
+	}
+	
+	protected boolean isSelectionMouseListeningDesired() {
+		return isActuallyPotentiallySelectable();
+	}
+	protected boolean isActuallyPotentiallySelectable() {
+		return true;
+	}
+
+	private float clickThreshold = 5.0f;
+	
+	public float getClickThreshold() {
+		return this.clickThreshold;
+	}
+	public void setClickThreshold( float clickThreshold ) {
+		this.clickThreshold = clickThreshold;
+	}
+	
+	private boolean isWithinClickThreshold = false;
+	
+	//always seems a bit iffy to have a method and a field with the same name
+	protected boolean isWithinClickThreshold() {
+		return this.isWithinClickThreshold;
+	}
+	
+	private java.awt.event.MouseEvent mousePressedEvent = null;
+	public java.awt.event.MouseEvent getMousePressedEvent() {
+		return this.mousePressedEvent;
+	}
+	protected void handleControlClick( java.awt.event.MouseEvent e ) {
+	}
+	protected void handleMousePressed( java.awt.event.MouseEvent e ) {
+		this.mousePressedEvent = e;
+		this.isWithinClickThreshold = true;
+	}
+	protected void handleMouseReleased( java.awt.event.MouseEvent e ) {
+		if( this.isWithinClickThreshold ) {
+			if( this.isActuallyPotentiallySelectable() ) {
+//				if( getIDE().isDragInProgress() ) {
+//					//
+//				} else {
+					if( edu.cmu.cs.dennisc.swing.SwingUtilities.isQuoteControlUnquoteDown( e ) ) {
+						this.handleControlClick( e );
+					} else {
+						this.setSelected( !ZControl.this.isSelected() );
+					}
+//				}
+			}
+		}
+		this.mousePressedEvent = null;
+		this.isWithinClickThreshold = false;
+	}
+	protected void handleMouseDraggedOutsideOfClickThreshold( java.awt.event.MouseEvent e ) {
+		this.isWithinClickThreshold = false;
+	}
+	protected void handleMouseDragged( java.awt.event.MouseEvent e ) {
+		if( this.isWithinClickThreshold ) {
+			int dx = e.getX() - this.mousePressedEvent.getX();
+			int dy = e.getY() - this.mousePressedEvent.getY();
+			if( dx*dx + dy*dy > this.clickThreshold*this.clickThreshold ) {
+				handleMouseDraggedOutsideOfClickThreshold( e );
+			}
+		}
+	}
+	
+
+	public boolean isSelected() {
+		return this.isSelected;
+	}
+	public void setSelected( boolean isSelected ) {
+		this.isSelected = isSelected;
+		if( this.isSelected ) {
+			this.requestFocus();
+		}
+		this.repaint();
+	}
+	
+}
---
+++ x_zoot/src/zoot/ZManager.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2006-2009, Carnegie Mellon University. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * 3. Products derived from the software may not be called "Alice",
+ *    nor may "Alice" appear in their name, without prior written
+ *    permission of Carnegie Mellon University.
+ * 
+ * 4. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *    "This product includes software developed by Carnegie Mellon University"
+ */
+package zoot;
+
+/**
+ * @author Dennis Cosgrove
+ */
+public class ZManager {
+	static {
+	}
+//	public static void setHandler( Handler handler ) {
+//	}
+	public static void handleTabSelection( ZTabbedPane tabbedPane, int index ) {
+	}
+//	private static void addToHistory( Operation operation ) {
+//	}
+//	private static void handlePreparedOperation( CancellableOperation operation, java.util.EventObject e, java.util.List< java.util.EventObject > preparationUpdates, CancellableOperation.PreparationResult preparationResult ) {
+//		edu.cmu.cs.dennisc.print.PrintUtilities.println( "ZManager.handlePreparedOperation", operation, preparationResult );
+//		if( preparationResult != null ) {
+//			if( preparationResult == CancellableOperation.PreparationResult.CANCEL ) {
+//				//pass
+//			} else {
+//				operation.perform();
+//				if( preparationResult == CancellableOperation.PreparationResult.PERFORM_AND_ADD_TO_HISTORY ) {
+//					//addToHistory( operation );
+//				}
+//			}
+//		}
+//	}
+	public static void performIfAppropriate( Operation operation, final java.util.EventObject e ) {
+		Context context = new Context() {
+			private java.util.Map< Object, Object > map = new java.util.HashMap< Object, Object >();
+			private boolean isCommitted = false;
+			private boolean isCancelled = false;
+			public boolean isCommitted() {
+				return this.isCommitted;
+			}
+			public boolean isCancelled() {
+				return this.isCancelled;
+			}
+			public boolean isPending() {
+				return ( this.isCommitted() || this.isCancelled() ) == false;
+			}
+			public void cancel() {
+				assert this.isPending();
+				this.isCancelled = true;
+			}
+			public void commit() {
+				assert this.isPending();
+				this.isCommitted = true;
+			}
+			public <E extends Object> E get( Object key, Class<E> cls ) {
+				Object rv = this.get( key );
+				if( rv != null ) {
+					assert cls.isAssignableFrom( rv.getClass() );
+					return (E)rv;
+				} else {
+					return null;
+				}
+			}
+			public Object get( Object key ) {
+				return this.map.get( key );
+			}
+			public void put( Object key, Object value ) {
+				this.map.put( key, value );
+			}
+			public java.util.EventObject getEvent() {
+				return e;
+			}
+			public Context perform( Operation operation, java.util.EventObject o ) {
+				return null;
+			}
+			
+		};
+		operation.perform( context );
+		
+//		if( operation instanceof CancellableOperation ) {
+//			CancellableOperation cancellableOperation = (CancellableOperation)operation;
+//			final java.util.List< java.util.EventObject > preparationUpdates = new java.util.LinkedList< java.util.EventObject >();
+//			CancellableOperation.PreparationResult preparationResult = cancellableOperation.prepare( e, new CancellableOperation.PreparationObserver() {
+//				public void update( java.util.EventObject e ) {
+//					preparationUpdates.add( e );
+//				}
+//			} );
+//			handlePreparedOperation( cancellableOperation, e, preparationUpdates, preparationResult );
+//		} else {
+//			if( operation instanceof ResponseOperation ) {
+//				((ResponseOperation)operation).respond( e );
+//			}
+//			operation.perform();
+//		}
+	}
+}
