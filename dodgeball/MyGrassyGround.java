b525c6997e1ad39bf1b495dfc4780aa455ef950a
bchen <bchen@c85dc8a7-84c8-304b-b635-f907e77af1ba>
2009-06-12 18:46:39 +0000

    git-svn-id: https://alma.andrew.cmu.edu:8443/svn/alice3/trunk@823 c85dc8a7-84c8-304b-b635-f907e77af1ba

--- dodgeball/MyGrassyGround.java
+++
@@ -1,9 +0,0 @@
-
-import org.alice.apis.moveandturn.gallery.environments.grounds.GrassyGround;
-import org.alice.apis.moveandturn.*;
-
-public class MyGrassyGround extends GrassyGround {
-
-    public MyGrassyGround() {
-    }
-}
--- dodgeball/MySunLight.java
+++
@@ -1,8 +0,0 @@
-
-import org.alice.apis.moveandturn.*;
-
-public class MySunLight extends DirectionalLight {
-
-    public MySunLight() {
-    }
-}
