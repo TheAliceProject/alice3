716f4ad0fdebd8053f1355d253703a2f94bd223b
dennisc <dennisc@c85dc8a7-84c8-304b-b635-f907e77af1ba>
2009-08-11 20:00:32 +0000

    git-svn-id: https://alma.andrew.cmu.edu:8443/svn/alice3/trunk@997 c85dc8a7-84c8-304b-b635-f907e77af1ba

--- x_progress/src/edu/cmu/cs/dennisc/progress/ProgressDialog.java
+++ x_progress/src/edu/cmu/cs/dennisc/progress/ProgressDialog.java
@@ -1,54 +1,134 @@
 package edu.cmu.cs.dennisc.progress;
 
 public abstract class ProgressDialog extends javax.swing.JDialog {
-	public ProgressDialog( javax.swing.JDialog owner ) {
-		super( owner );
-		this.initialize();
-	}
-	public ProgressDialog( javax.swing.JFrame owner ) {
-		super( owner );
-		this.initialize();
-	}
-	protected abstract Boolean doInBackground( org.jdesktop.swingworker.SwingWorker< Boolean, String > worker ) throws java.lang.Exception;
-	private void initialize() {
-		org.jdesktop.swingworker.SwingWorker< Boolean, String > worker = new org.jdesktop.swingworker.SwingWorker< Boolean, String >() {
-			@Override
-			protected Boolean doInBackground() throws java.lang.Exception {
-				return ProgressDialog.this.doInBackground( this );
+	private javax.swing.JTextArea textArea = new javax.swing.JTextArea();
+	private javax.swing.JProgressBar progressBar = new javax.swing.JProgressBar();
+	protected abstract class Worker extends org.jdesktop.swingworker.SwingWorker<Boolean, String> {
+		protected void setPortionCompleted(double portionCompleted) {
+			this.setProgress((int) (100 * portionCompleted));
+		}
+
+		@Override
+		protected void process(java.util.List<String> texts) {
+			super.process(texts);
+			for (String text : texts) {
+				ProgressDialog.this.textArea.append(text);
 			}
-			@Override
-			protected void process( java.util.List< String > texts ) {
-				super.process( texts );
-				for( String text : texts ) {
-					System.out.println( text );
-				}
-			}
-		};
-		worker.execute();
-		try {
-			Boolean isSuccessful = worker.get();
-			System.out.println( "isSuccessful: " + isSuccessful );
-		} catch( Exception e ) {
-			e.printStackTrace();
 		}
 	}
-	//protected abstract org.jdesktop.swingworker.SwingWorker< T, V > createWorker();
-	public static void main( String[] args ) {
-		javax.swing.JFrame frame = new javax.swing.JFrame();
-		ProgressDialog dialog = new ProgressDialog( frame ) {
-			@Override
-			protected Boolean doInBackground( org.jdesktop.swingworker.SwingWorker< Boolean, String > worker ) throws java.lang.Exception {
-				final int N = 10;
-				for( int i = 0; i < N; i++ ) {
-//					worker.setProgress( i );
-//					worker.publish( Integer.toString( i ) );
-					Thread.sleep( 100 );
-				}
-//				worker.setProgress( N );
-				return true;
+	private java.beans.PropertyChangeListener propertyChangeListener = new java.beans.PropertyChangeListener() {
+		public void propertyChange(java.beans.PropertyChangeEvent e) {
+			String propertyName = e.getPropertyName();
+			Object propertyValue = e.getNewValue();
+			if ("progress".equals( propertyName )) {
+				ProgressDialog.this.handleProgressChange((Integer)propertyValue);
+			} else if ("state".equals( propertyName )) {
+				ProgressDialog.this.handleStateChange( (Worker)e.getSource(), (org.jdesktop.swingworker.SwingWorker.StateValue)propertyValue);
+			} else {
+				System.out.println(propertyName + " " + propertyValue);
 			}
-		};
-		dialog.setVisible( true );
+		}
+	};
+	public ProgressDialog(javax.swing.JDialog owner) {
+		super(owner);
+	}
+	public ProgressDialog(javax.swing.JFrame owner) {
+		super(owner);
 	}
 
+	protected void addComponentsToPageAxisContentPane( java.awt.Container contentPane ) {
+		javax.swing.JScrollPane scrollPane = new javax.swing.JScrollPane(this.textArea) {
+			@Override
+			public java.awt.Dimension getPreferredSize() {
+				return new java.awt.Dimension(320, 240);
+			}
+		};
+		contentPane.add(scrollPane);
+		if (this.isProgressBarDesired()) {
+			contentPane.add(this.progressBar);
+		}
+	}
+	@Override
+	public void addNotify() {
+		super.addNotify();
+		java.awt.Container contentPane = this.getContentPane();
+		if( contentPane.getComponentCount() == 0 ) {
+			contentPane.setLayout( new javax.swing.BoxLayout( contentPane, javax.swing.BoxLayout.PAGE_AXIS ) );
+			addComponentsToPageAxisContentPane( contentPane );
+		}
+//		edu.cmu.cs.dennisc.print.PrintUtilities.println( "addNotify" );
+	}
+	@Override
+	public void removeNotify() {
+//		edu.cmu.cs.dennisc.print.PrintUtilities.println( "removeNotify" );
+		super.removeNotify();
+	}
+
+	protected abstract Worker createWorker();
+	protected abstract void handleDone( Boolean result );
+	protected abstract boolean isProgressBarDesired();
+
+	protected void createAndExecuteWorker() {
+		Worker worker = createWorker();
+		worker.addPropertyChangeListener(this.propertyChangeListener);
+		worker.execute();
+	}
+	protected void handleProgressChange( Integer progress ) {
+		this.progressBar.setValue(progress);
+	}
+	protected void handleStateChange( Worker worker, org.jdesktop.swingworker.SwingWorker.StateValue state ) {
+		if( org.jdesktop.swingworker.SwingWorker.StateValue.DONE.equals( state ) ) {
+			try {
+				worker.removePropertyChangeListener( this.propertyChangeListener );
+				Boolean result = worker.get();
+				if( result ) {
+					this.progressBar.setValue(100);
+				}
+				this.handleDone(result);
+			} catch( Exception e ) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				javax.swing.JFrame frame = new javax.swing.JFrame();
+				ProgressDialog dialog = new ProgressDialog(frame) {
+					@Override
+					protected boolean isProgressBarDesired() {
+						return true;
+					}
+					
+					@Override
+					protected void handleDone(Boolean result) {
+						if( result ) {
+							this.setVisible(false);
+						}
+					}
+		
+					@Override
+					protected edu.cmu.cs.dennisc.progress.ProgressDialog.Worker createWorker() {
+						return new Worker() {
+							@Override
+							protected Boolean doInBackground() throws Exception {
+								final int N = 10;
+								for (int i = 0; i < N; i++) {
+									this.setPortionCompleted(i / (double)N);
+									this.publish(Integer.toString(i) + "\n");
+									Thread.sleep(100);
+								}
+								this.publish("complete");
+								return true;
+							}
+						};
+					}
+				};
+				dialog.createAndExecuteWorker();
+				dialog.pack();
+				dialog.setVisible(true);
+			}
+		} );
+	}
 }
