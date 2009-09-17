92ae3eb0a8ad77c5335126bf9153ebd69e7e4e1a
dculyba <dculyba@c85dc8a7-84c8-304b-b635-f907e77af1ba>
2009-09-17 18:23:36 +0000

    git-svn-id: https://alma.andrew.cmu.edu:8443/svn/alice3/branches/3.0.0.0.63@1041 c85dc8a7-84c8-304b-b635-f907e77af1ba

--- x_ide_stage/src/org/alice/stageide/EntryPoint.java
+++ x_ide_stage/src/org/alice/stageide/EntryPoint.java
@@ -35,7 +35,7 @@ class SplashIcon extends javax.swing.ImageIcon {
 		int w = this.getIconWidth();
 		int h = this.getIconHeight();
 		g.setColor( new java.awt.Color( 0x379bd5 ) );
-		g.drawString( edu.cmu.cs.dennisc.alice.Version.getCurrentVersionText() + " BETA", x + (int)(w*0.45), y + (int)(h*0.75) );
+		g.drawString( edu.cmu.cs.dennisc.alice.Version.getCurrentVersionText()/* + " BETA"*/, x + (int)(w*0.45), y + (int)(h*0.75) );
 	}
 }
 
@@ -44,7 +44,7 @@ class SplashIcon extends javax.swing.ImageIcon {
  */
 public class EntryPoint {
 	public static void main( final String[] args ) {
-		System.out.println( "version: " + edu.cmu.cs.dennisc.alice.Version.getCurrentVersionText() + " BETA" );
+		System.out.println( "version: " + edu.cmu.cs.dennisc.alice.Version.getCurrentVersionText()/* + " BETA" */);
 		javax.swing.Icon icon = new SplashIcon( EntryPoint.class.getResource( "images/SplashScreen.png" ) );
 		java.awt.Window splashScreen = new edu.cmu.cs.dennisc.swing.SplashScreen( null, icon );
 		org.alice.ide.LaunchUtilities.launch( StageIDE.class, splashScreen, args );
