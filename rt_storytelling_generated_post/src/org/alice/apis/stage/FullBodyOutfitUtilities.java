1cc49baabdb24a748d5412361d5fa60699284edc
dennisc <dennisc@c85dc8a7-84c8-304b-b635-f907e77af1ba>
2010-02-11 00:44:49 +0000

    git-svn-id: https://alma.andrew.cmu.edu:8443/svn/alice3/trunk@1203 c85dc8a7-84c8-304b-b635-f907e77af1ba

---
+++ rt_storytelling_generated_post/src/org/alice/apis/stage/FullBodyOutfitUtilities.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright (c) 2006-2009, Carnegie Mellon University. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * 3. Products derived from the software may not be called "Alice",
+ *    nor may "Alice" appear in their name, without prior written
+ *    permission of Carnegie Mellon University.
+ * 
+ * 4. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *    "This product includes software developed by Carnegie Mellon University"
+ */
+package org.alice.apis.stage;
+
+public class FullBodyOutfitUtilities extends IngredientUtilities< FullBodyOutfit > {
+	private static FullBodyOutfitUtilities singleton = new FullBodyOutfitUtilities();
+
+	public static FullBodyOutfitUtilities getSingleton() {
+		return singleton;
+	}
+
+	private FullBodyOutfitUtilities() {
+		this.add( AdultFullBodyOutfit.class,
+							UnisexAdultFullBodyOutfitAstronaut.class ,
+							UnisexAdultFullBodyOutfitLlama.class ,
+							UnisexAdultFullBodyOutfiteton.class
+		);
+
+		this.add( FemaleAdultFullBodyOutfit.class,
+							FemaleAdultFullBodyOutfitAmbulanceDriver.class ,
+							FemaleAdultFullBodyOutfitApron.class ,
+							FemaleAdultFullBodyOutfitBartender.class ,
+							FemaleAdultFullBodyOutfitBurglar.class ,
+							FemaleAdultFullBodyOutfitBusDriver.class ,
+							FemaleAdultFullBodyOutfitChef.class ,
+							FemaleAdultFullBodyOutfitClerk.class ,
+							FemaleAdultFullBodyOutfitCoach.class ,
+							FemaleAdultFullBodyOutfitCop.class ,
+							FemaleAdultFullBodyOutfitDeliveryPerson.class ,
+							FemaleAdultFullBodyOutfitDress.class ,
+							FemaleAdultFullBodyOutfitDressAboveKnee.class ,
+							FemaleAdultFullBodyOutfitDressAboveKneeHooded.class ,
+							FemaleAdultFullBodyOutfitDressAboveKneeSuit.class ,
+							FemaleAdultFullBodyOutfitDressChina.class ,
+							FemaleAdultFullBodyOutfitDressFormalLong.class ,
+							FemaleAdultFullBodyOutfitDressGothic.class ,
+							FemaleAdultFullBodyOutfitDressKorean.class ,
+							FemaleAdultFullBodyOutfitDressLongHug.class ,
+							FemaleAdultFullBodyOutfitDressLongLoose.class ,
+							////FemaleAdultFullBodyOutfitDressLongTwo.class ,
+							//FemaleAdultFullBodyOutfitExterminator.class ,
+							FemaleAdultFullBodyOutfitFastFood.class ,
+							FemaleAdultFullBodyOutfitFurCoatShortDressShoes.class ,
+							FemaleAdultFullBodyOutfitGardener.class ,
+							FemaleAdultFullBodyOutfitJacketHighCollar.class ,
+							FemaleAdultFullBodyOutfitJacketShortDressBoots.class ,
+							FemaleAdultFullBodyOutfitJacketShortDressShoes.class ,
+							FemaleAdultFullBodyOutfitJacketShortDressSlitShoes.class ,
+							FemaleAdultFullBodyOutfitJacketTurtleSweaterDressBoots.class ,
+							FemaleAdultFullBodyOutfitKTDream.class ,
+							FemaleAdultFullBodyOutfitLabcoat.class ,
+							FemaleAdultFullBodyOutfitLeatherJacket.class ,
+							FemaleAdultFullBodyOutfitMailDelivery.class ,
+							FemaleAdultFullBodyOutfitMaternityShirtPants.class ,
+							FemaleAdultFullBodyOutfitMechanic.class ,
+							FemaleAdultFullBodyOutfitMilitaryOfficer.class ,
+							FemaleAdultFullBodyOutfitMRacer.class ,
+							//FemaleAdultFullBodyOutfitNaked.class ,
+							FemaleAdultFullBodyOutfitNightgown.class ,
+							FemaleAdultFullBodyOutfitOrderly.class ,
+							FemaleAdultFullBodyOutfitOverShirtPantsSandals.class ,
+							FemaleAdultFullBodyOutfitPajamasClassic.class ,
+							FemaleAdultFullBodyOutfitPirate.class ,
+							FemaleAdultFullBodyOutfitPizzaDelivery.class ,
+							FemaleAdultFullBodyOutfitPowerSuit.class ,
+							FemaleAdultFullBodyOutfitScrubs.class ,
+							FemaleAdultFullBodyOutfitServer.class ,
+							FemaleAdultFullBodyOutfitShirtUntuckedOxford.class ,
+							////FemaleAdultFullBodyOutfitShortDressBoots.class ,
+							//FemaleAdultFullBodyOutfitShortDressShoes.class ,
+							FemaleAdultFullBodyOutfitSlickSuit.class ,
+							FemaleAdultFullBodyOutfitSocialWorker.class ,
+							FemaleAdultFullBodyOutfitSoldier.class ,
+							FemaleAdultFullBodyOutfitSuit.class ,
+							FemaleAdultFullBodyOutfitSundress.class ,
+							////FemaleAdultFullBodyOutfitSuspendersTights.class ,
+							FemaleAdultFullBodyOutfitSwat.class ,
+							FemaleAdultFullBodyOutfitSweats.class ,
+							////FemaleAdultFullBodyOutfitSwimwear.class ,
+							////FemaleAdultFullBodyOutfitSwimwearCleavage.class ,
+							////FemaleAdultFullBodyOutfitSwimwearSporty.class ,
+							FemaleAdultFullBodyOutfitTracksuit.class ,
+							FemaleAdultFullBodyOutfitTurtleSweaterDressShoes.class ,
+							FemaleAdultFullBodyOutfitTweedJacket.class
+							////FemaleAdultFullBodyOutfitUnderwear.class ,
+							////FemaleAdultFullBodyOutfitWarrior.class ,
+		);
+		this.add( MaleAdultFullBodyOutfit.class,
+							MaleAdultFullBodyOutfitAmbulanceDriver.class ,
+							MaleAdultFullBodyOutfitApron.class ,
+							MaleAdultFullBodyOutfitBartender.class ,
+							MaleAdultFullBodyOutfitBurglar.class ,
+							MaleAdultFullBodyOutfitBusDriver.class ,
+							MaleAdultFullBodyOutfitChef.class ,
+							MaleAdultFullBodyOutfitClerk.class ,
+							MaleAdultFullBodyOutfitClosedCoatLongPants.class ,
+							MaleAdultFullBodyOutfitCoach.class ,
+							MaleAdultFullBodyOutfitCop.class ,
+							MaleAdultFullBodyOutfitCouture.class ,
+							MaleAdultFullBodyOutfitDeliveryPerson.class ,
+							MaleAdultFullBodyOutfitDressKorean.class ,
+							MaleAdultFullBodyOutfitExterminator.class ,
+							MaleAdultFullBodyOutfitFancySuit.class ,
+							MaleAdultFullBodyOutfitFastFood.class ,
+							MaleAdultFullBodyOutfitGardener.class ,
+							MaleAdultFullBodyOutfitGothTeeShirt.class ,
+							MaleAdultFullBodyOutfitHiphopHood.class ,
+							MaleAdultFullBodyOutfitHoodedSweatShirtBoardShorts.class ,
+							MaleAdultFullBodyOutfitHoodedSweatShirtPants.class ,
+							MaleAdultFullBodyOutfitKilt.class ,
+							MaleAdultFullBodyOutfitLabcoat.class ,
+							MaleAdultFullBodyOutfitLongCoat.class ,
+							MaleAdultFullBodyOutfitLooseOpenCoatPants.class ,
+							MaleAdultFullBodyOutfitMailDelivery.class ,
+							MaleAdultFullBodyOutfitManMaid.class ,
+							MaleAdultFullBodyOutfitMaternityComfy.class ,
+							MaleAdultFullBodyOutfitMechanic.class ,
+							MaleAdultFullBodyOutfitMRacer.class ,
+							//MaleAdultFullBodyOutfitNaked.class ,
+							MaleAdultFullBodyOutfitOpenCoatJeans.class ,
+							MaleAdultFullBodyOutfitOpenCoatLongPants.class ,
+							MaleAdultFullBodyOutfitOpenShirtPants.class ,
+							MaleAdultFullBodyOutfitOpenSportcoatLongPants.class ,
+							MaleAdultFullBodyOutfitOrderly.class ,
+							MaleAdultFullBodyOutfitOveralls.class ,
+							MaleAdultFullBodyOutfitOverhangTshirt.class ,
+							//MaleAdultFullBodyOutfitOverhangTshirtGamespot.class ,
+							//MaleAdultFullBodyOutfitOverhangTshirtGamespy.class ,
+							MaleAdultFullBodyOutfitOverhangTshirtLongShorts.class ,
+							MaleAdultFullBodyOutfitOverShirtPantsShoes.class ,
+							MaleAdultFullBodyOutfitPajamas.class ,
+							MaleAdultFullBodyOutfitPajamasBoxers.class ,
+							MaleAdultFullBodyOutfitPajamasDrawstring.class ,
+							MaleAdultFullBodyOutfitPirate.class ,
+							MaleAdultFullBodyOutfitPizzaDelivery.class ,
+							MaleAdultFullBodyOutfitPowerSuit.class ,
+							MaleAdultFullBodyOutfitPunk.class ,
+							MaleAdultFullBodyOutfitScrubs.class ,
+							MaleAdultFullBodyOutfitServer.class ,
+							MaleAdultFullBodyOutfitShirtFlares.class ,
+							MaleAdultFullBodyOutfitShirtUntuckedSaddle.class ,
+							MaleAdultFullBodyOutfitShorts.class ,
+				//			MaleAdultFullBodyOutfitShrink.class ,
+							MaleAdultFullBodyOutfitSlickSuit.class ,
+							MaleAdultFullBodyOutfitSloppySuit.class ,
+							MaleAdultFullBodyOutfitSoldier.class ,
+							MaleAdultFullBodyOutfitSuit.class ,
+							MaleAdultFullBodyOutfitSwat.class ,
+							MaleAdultFullBodyOutfitSweats.class ,
+							MaleAdultFullBodyOutfitTrackSuit.class ,
+							MaleAdultFullBodyOutfitTrenchCoatPantsBoots.class ,
+							MaleAdultFullBodyOutfitTweedJacket.class
+							////MaleAdultFullBodyOutfitUnderwear.class ,
+		);
+	}
+
+	@Override
+	protected Class< ? extends FullBodyOutfit > getUnisexIntefaceClass( LifeStage lifeStage ) {
+		return lifeStage.getUnisexFullBodyOutfitInterfaceClass();
+	};
+	@Override
+	protected Class< ? extends FullBodyOutfit > getGenderedIntefaceClass( LifeStage lifeStage, Gender gender ) {
+		return lifeStage.getGenderedFullBodyOutfitInterfaceClass( gender );
+	}
+}
---
+++ rt_storytelling_generated_post/src/org/alice/apis/stage/HairUtilities.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2006-2009, Carnegie Mellon University. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * 3. Products derived from the software may not be called "Alice",
+ *    nor may "Alice" appear in their name, without prior written
+ *    permission of Carnegie Mellon University.
+ * 
+ * 4. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *    "This product includes software developed by Carnegie Mellon University"
+ */
+package org.alice.apis.stage;
+
+public class HairUtilities extends IngredientUtilities< Hair > {
+	private static HairUtilities singleton = new HairUtilities();
+
+	public static HairUtilities getSingleton() {
+		return singleton;
+	}
+
+	private HairUtilities() {
+		this.add( MaleAdultHair.class,
+						//MaleAdultHairBeanie.class ,
+						MaleAdultHairCloseCrop.class ,
+						MaleAdultHairCornRows.class ,
+						MaleAdultHairCrewCut.class ,
+						//MaleAdultHairCurlyWild.class ,
+						MaleAdultHairDreadlockLong.class ,
+						MaleAdultHairGibs.class ,
+						//MaleAdultHairHatFedora.class ,
+						MaleAdultHairHatFedoraCasual.class ,
+						MaleAdultHairMulletLong.class ,
+						MaleAdultHairPeak.class ,
+						MaleAdultHairPompodore.class ,
+						MaleAdultHairSemiBald.class ,
+						//MaleAdultHairShocked.class ,
+						MaleAdultHairShortCombed.class ,
+						MaleAdultHairShortSpikey.class ,
+						MaleAdultHairTopHat.class
+						//MaleAdultHairBald.class
+		);
+		this.add( FemaleAdultHair.class,
+	//					FemaleAdultHairBeanie.class ,
+						FemaleAdultHairBraids.class ,
+						FemaleAdultHairCornRowsLong.class ,
+						FemaleAdultHairDreadlockShort.class ,
+						FemaleAdultHairFeather.class ,
+						FemaleAdultHairGetFabulous.class ,
+						FemaleAdultHairPoofs.class ,
+						FemaleAdultHairRosettes.class ,
+						FemaleAdultHairShocked.class ,
+						FemaleAdultHairShortSlick.class 
+	//					FemaleAdultHairBald.class
+		);
+	}
+
+	@Override
+	protected Class< ? extends Hair > getUnisexIntefaceClass( LifeStage lifeStage ) {
+		return lifeStage.getUnisexHairInterfaceClass();
+	};
+	@Override
+	protected Class< ? extends Hair > getGenderedIntefaceClass( LifeStage lifeStage, Gender gender ) {
+		return lifeStage.getGenderedHairInterfaceClass( gender );
+	}
+}
