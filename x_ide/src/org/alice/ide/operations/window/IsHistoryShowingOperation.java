1c145e9144646e24ec2848ee091202a34046627d
dennisc <dennisc@c85dc8a7-84c8-304b-b635-f907e77af1ba>
2010-03-01 20:16:49 +0000

    git-svn-id: https://alma.andrew.cmu.edu:8443/svn/alice3/trunk@1315 c85dc8a7-84c8-304b-b635-f907e77af1ba

--- x_ide/src/org/alice/ide/operations/window/IsHistoryShowingOperation.java
+++ x_ide/src/org/alice/ide/operations/window/IsHistoryShowingOperation.java
@@ -3,7 +3,7 @@ package org.alice.ide.operations.window;
 public class IsHistoryShowingOperation extends IsFrameShowingOperation {
 	public IsHistoryShowingOperation() {
 		//todo: PREFERENCES_GROUP?
-		super( org.alice.ide.IDE.INTERFACE_GROUP, true );
+		super( org.alice.ide.IDE.INTERFACE_GROUP, false );
 		this.putValue( javax.swing.Action.NAME, "Show History?" );
 	}
 	@Override
