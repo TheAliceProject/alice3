0c69547065b65eea9476fc81203e7399d21e2d71
dculyba <dculyba@c85dc8a7-84c8-304b-b635-f907e77af1ba>
2010-03-10 22:19:16 +0000

    git-svn-id: https://alma.andrew.cmu.edu:8443/svn/alice3/branches/3.0.0.1.1@1430 c85dc8a7-84c8-304b-b635-f907e77af1ba

---
+++ lg_walkandtouch/src/edu/wustl/cse/lookingglass/apis/walkandtouch/animation/LookDirectionAnimation.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2008-2010, Washington University in St. Louis. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Products derived from the software may not be called "Looking Glass", nor 
+ *    may "Looking Glass" appear in their name, without prior written permission
+ *    of Washington University in St. Louis.
+ *
+ * 4. All advertising materials mentioning features or use of this software must
+ *    display the following acknowledgement: "This product includes software 
+ *    developed by Washington University in St. Louis"
+ *
+ * 5. The gallery of art assets and animations provided with this software is 
+ *    contributed by Electronic Arts Inc. and may be used for personal, 
+ *    non-commercial, and academic use only. Redistributions of any program 
+ *    source code that utilizes The Sims 2 Assets must also retain the copyright
+ *    notice, list of conditions and the disclaimer contained in 
+ *    The Alice 3.0 Art Gallery License.
+ *
+ * DISCLAIMER:
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.  ANY AND ALL 
+ * EXPRESS, STATUTORY OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY,  FITNESS FOR A PARTICULAR PURPOSE, 
+ * TITLE, AND NON-INFRINGEMENT ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS, 
+ * COPYRIGHT OWNERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ * INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING FROM OR OTHERWISE RELATING TO 
+ * THE USE OF OR OTHER DEALINGS WITH THE SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.wustl.cse.lookingglass.apis.walkandtouch.animation;
+
+import org.alice.apis.moveandturn.AsSeenBy;
+import org.alice.apis.moveandturn.HowMuch;
+
+import edu.cmu.cs.dennisc.math.UnitQuaternion;
+import edu.wustl.cse.lookingglass.apis.walkandtouch.LookDirection;
+
+
+/**
+ * @author caitlink
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class LookDirectionAnimation extends edu.cmu.cs.dennisc.animation.DurationBasedAnimation {
+	LookDirection lookDirection = LookDirection.FORWARD;
+	double amount = 0.2;	
+	edu.wustl.cse.lookingglass.apis.walkandtouch.PolygonalModel head = null;
+	edu.wustl.cse.lookingglass.apis.walkandtouch.PolygonalModel subject = null;
+	
+	edu.cmu.cs.dennisc.math.OrthogonalMatrix3x3 initialAxes = null;
+	edu.cmu.cs.dennisc.math.OrthogonalMatrix3x3 targetAxes = null;
+	
+	public LookDirectionAnimation(edu.wustl.cse.lookingglass.apis.walkandtouch.PolygonalModel subject, LookDirection lookDirection, double amount, double duration) {
+		super(duration);
+		this.subject = subject;
+		this.lookDirection = lookDirection;
+		this.amount = amount;
+	}
+	
+	@Override
+	public void prologue(  ) {			
+		findHead();		
+		if (head != null) {
+			initialAxes = head.getOrientation( org.alice.apis.moveandturn.AsSeenBy.PARENT );
+		} else {
+			initialAxes = subject.getOrientation(AsSeenBy.PARENT);
+		}
+		targetAxes = null;
+		
+	}
+	
+	@Override
+	protected void setPortion( double portion ) {
+		if (targetAxes == null) 
+			targetAxes = getTargetMatrix();
+		
+		UnitQuaternion currentQuat = UnitQuaternion.createInterpolation(initialAxes.createUnitQuaternion(), targetAxes.createUnitQuaternion(), portion);
+		
+		if (head != null) {
+			head.setOrientationRightNow( currentQuat.createOrthogonalMatrix3x3(), org.alice.apis.moveandturn.AsSeenBy.PARENT );
+		} else {
+			subject.setOrientationRightNow( currentQuat.createOrthogonalMatrix3x3(), org.alice.apis.moveandturn.AsSeenBy.PARENT );
+		}
+	}
+	
+	
+	
+	@Override
+	protected void epilogue() {
+		// TODO Auto-generated method stub		
+	}
+	
+	public edu.cmu.cs.dennisc.math.OrthogonalMatrix3x3 getTargetMatrix() {	
+
+		edu.cmu.cs.dennisc.math.OrthogonalMatrix3x3 headOrient = null;
+		if (head != null) {
+			headOrient = new edu.cmu.cs.dennisc.math.OrthogonalMatrix3x3(); 
+		} else {
+			headOrient = initialAxes.createOrthogonalMatrix3x3();
+		}	
+		
+		if (lookDirection == LookDirection.FORWARD) {		
+		} else if (lookDirection == LookDirection.UP) {
+			edu.wustl.cse.lookingglass.apis.walkandtouch.RotationUtilities.rotateAroundXInRevolutions( headOrient, -1.0* amount);
+		} else if (lookDirection == LookDirection.DOWN) {
+			edu.wustl.cse.lookingglass.apis.walkandtouch.RotationUtilities.rotateAroundXInRevolutions(headOrient, amount);
+		} else if (lookDirection == LookDirection.LEFT) {
+			edu.wustl.cse.lookingglass.apis.walkandtouch.RotationUtilities.rotateAroundYInRevolutions(headOrient, -1.0 * amount);
+		} else if (lookDirection == LookDirection.RIGHT) {
+			edu.wustl.cse.lookingglass.apis.walkandtouch.RotationUtilities.rotateAroundYInRevolutions(headOrient, amount);
+		}
+
+		return headOrient;
+	}
+	
+	public void findHead() {
+//		edu.cmu.cs.dennisc.print.PrintUtilities.printlnsTree( subject, false );
+		head = subject.findFirstMatch( HowMuch.DESCENDANT_PARTS_ONLY, edu.wustl.cse.lookingglass.apis.walkandtouch.PolygonalModel.class, new edu.wustl.cse.lookingglass.apis.walkandtouch.pattern.NameContainsCriterion(
+				"head", false ) );
+	}
+
+}
