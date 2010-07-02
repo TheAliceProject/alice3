4af586f13a50f2892271b493ce839ad1fb68f81a
Dennis Cosgrove <dennisc@cs.cmu.edu>
2010-07-02 01:31:19 -0400

    rendering
    Former-commit-id: e66f74700ce89c34bfb3797c313d7541b9385f1c

--- ide/src/org/alice/ide/memberseditor/MembersEditor.java
+++ ide/src/org/alice/ide/memberseditor/MembersEditor.java
@@ -257,6 +257,13 @@ public class MembersEditor extends edu.cmu.cs.dennisc.croquet.BorderPanel {
 					String className = valueTypeInJava.getClassReflectionProxy().getName();
 					
 					rv.setIcon( org.alice.stageide.gallerybrowser.ResourceManager.getSmallIconForGalleryClassName(className));
+					
+//					rv = new javax.swing.JLabel( rv.getText(), rv.getIcon(), javax.swing.SwingConstants.LEADING ) {
+//						@Override
+//						public boolean isOptimizedDrawingEnabled() {
+//							return false;
+//						}
+//					};
 				}
 				return rv;
 			}
