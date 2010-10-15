cf8e9e103f84c29dd4d71dafe186c4c3edbb31de
Dennis Cosgrove <dennisc@cs.cmu.edu>
2010-10-15 18:22:04 -0400

    tidy
    Former-commit-id: 62970438a20ded369ea77694cc3826bb3defbac4

--- ide/src/edu/cmu/cs/dennisc/croquet/WizardDialogOperation.java
+++ ide/src/edu/cmu/cs/dennisc/croquet/WizardDialogOperation.java
@@ -142,18 +142,24 @@ public abstract class WizardDialogOperation extends GatedCommitDialogOperation<W
 		}
 	};
 	
-	private final ListSelectionState< Card > cardSelectionState = new ListSelectionState< Card >( Application.INFORMATION_GROUP, java.util.UUID.fromString( "2382103d-a67e-4a35-baa2-9a612fd2d8f2" ), new Codec< Card >() {
-		public StringBuilder appendRepresentation( StringBuilder rv, Card value, java.util.Locale locale ) {
-			rv.append( value );
-			return rv;
+	private static class CardSelectionState extends ListSelectionState< Card > {
+		public CardSelectionState() {
+			super( DIALOG_IMPLEMENTATION_GROUP, java.util.UUID.fromString( "2382103d-a67e-4a35-baa2-9a612fd2d8f2" ), new Codec< Card >() {
+				public StringBuilder appendRepresentation( StringBuilder rv, Card value, java.util.Locale locale ) {
+					rv.append( value );
+					return rv;
+				}
+				public Card decode( edu.cmu.cs.dennisc.codec.BinaryDecoder binaryDecoder ) {
+					throw new RuntimeException( "todo" );
+				}
+				public void encode( edu.cmu.cs.dennisc.codec.BinaryEncoder binaryEncoder, Card card ) {
+					throw new RuntimeException( "todo" );
+				}
+			} );
 		}
-		public Card decode( edu.cmu.cs.dennisc.codec.BinaryDecoder binaryDecoder ) {
-			throw new RuntimeException( "todo" );
-		}
-		public void encode( edu.cmu.cs.dennisc.codec.BinaryEncoder binaryEncoder, Card card ) {
-			throw new RuntimeException( "todo" );
-		}
-	} );
+	}
+	
+	private final CardSelectionState cardSelectionState = new CardSelectionState();
 	
 	private class StepsProgressPanel extends PageAxisPanel {
 		public StepsProgressPanel() {
