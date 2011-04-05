e8a7ad85a413cd5836a0b4f6f7114d8361058e08
Dennis Cosgrove <dennisc@cs.cmu.edu>
2011-04-05 15:14:40 -0400

    code name in tutorial note
    Former-commit-id: b2de6dfb6f569f9da480cb4102dadb46a269d547

--- ide/src/org/alice/ide/editorstabbedpane/CodeCompositeCodec.java
+++ ide/src/org/alice/ide/editorstabbedpane/CodeCompositeCodec.java
@@ -70,7 +70,7 @@ public enum CodeCompositeCodec implements edu.cmu.cs.dennisc.croquet.Codec< Code
 		}
 	}
 	public StringBuilder appendRepresentation(StringBuilder rv, CodeComposite value, java.util.Locale locale) {
-		rv.append( value );
+		rv.append( value.getCode().getName() );
 		return rv;
 	}
 }
