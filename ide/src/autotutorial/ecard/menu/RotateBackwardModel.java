b78712d1cbaf8086b4853823050d289c2acee9bf
Kyle J. Harms <harmsk@seas.wustl.edu>
2011-04-21 20:51:48 -0500

    Icons for image effects
    Former-commit-id: 1929f1f51093845340d794b576a725dcfc950b6b

--- ide/src/autotutorial/ecard/menu/RotateBackwardModel.java
+++ ide/src/autotutorial/ecard/menu/RotateBackwardModel.java
@@ -1,4 +1,7 @@
 package autotutorial.ecard.menu;
+
+import javax.swing.ImageIcon;
+
 public class RotateBackwardModel extends edu.cmu.cs.dennisc.croquet.ActionOperation {
 	private static class SingletonHolder {
 		private static RotateBackwardModel instance = new RotateBackwardModel();
@@ -8,7 +11,7 @@ public class RotateBackwardModel extends edu.cmu.cs.dennisc.croquet.ActionOperat
 	}
 	private RotateBackwardModel() {
 		super( null, java.util.UUID.fromString( "88d087c4-a144-4418-9727-9ad752df5a6a" ) );
-		//this.setSmallIcon( new javax.swing.ImageIcon( RedoOperation.class.getResource( "images/undo.png" ) ) );
+		this.setSmallIcon( new ImageIcon(getClass().getResource("/autotutorial/ecard/resources/menu/rotate-counter-clockwise.png") ) );
 	}
 	@Override
 	protected void perform(edu.cmu.cs.dennisc.croquet.ActionOperationContext context) {
--- ide/src/autotutorial/ecard/menu/RoundedCornersModel.java
+++ ide/src/autotutorial/ecard/menu/RoundedCornersModel.java
@@ -1,5 +1,7 @@
 package autotutorial.ecard.menu;
 
+import javax.swing.ImageIcon;
+
 public class RoundedCornersModel extends edu.cmu.cs.dennisc.croquet.ActionOperation {
 	private static class SingletonHolder {
 		private static RoundedCornersModel instance = new RoundedCornersModel();
@@ -9,7 +11,7 @@ public class RoundedCornersModel extends edu.cmu.cs.dennisc.croquet.ActionOperat
 	}
 	private RoundedCornersModel() {
 		super( null, java.util.UUID.fromString( "3e8dec1d-8fec-41c3-961a-1828fbb17250" ) );
-		//this.setSmallIcon( new javax.swing.ImageIcon( RedoOperation.class.getResource( "images/undo.png" ) ) );
+		this.setSmallIcon( new ImageIcon(getClass().getResource("/autotutorial/ecard/resources/menu/rounded-corners.png") ) );
 	}
 	@Override
 	protected void perform(edu.cmu.cs.dennisc.croquet.ActionOperationContext context) {
