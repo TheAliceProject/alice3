574089ad5912820ea8588368e56f4b72c828c955
Dennis Cosgrove <dennisc@cs.cmu.edu>
2011-08-09 17:35:23 -0400

    tidy
    Former-commit-id: 46f5c10c9cf7c6fcb54d259a0e0dd38a368def98

--- ide/src/org/alice/stageide/StoryApiConfigurationManager.java
+++ ide/src/org/alice/stageide/StoryApiConfigurationManager.java
@@ -48,14 +48,6 @@ package org.alice.stageide;
  */
 public enum StoryApiConfigurationManager implements org.alice.ide.ApiConfigurationManager {
 	SINGLETON {
-		public boolean isFieldDeletable( org.lgna.project.ast.UserField field ) {
-			if( field.getValueType().isAssignableTo( org.lgna.story.Camera.class ) ) {
-				if( field.getDeclaringType().isAssignableTo( org.lgna.story.Scene.class ) ) {
-					return false;
-				}
-			}
-			return true;
-		}
 		public boolean isDeclaringTypeForManagedFields( org.lgna.project.ast.UserType< ? > type ) {
 			return type.isAssignableTo( org.lgna.story.Scene.class );
 		}
