8c29021dd85e0f21281802293b19ddb4a792d8c6
Dennis Cosgrove <dennisc@cs.cmu.edu>
2011-11-30 22:11:50 -0600

    show visualization
    Former-commit-id: e657cdc9b06acc293579cd5a3bb159eccdd7fe9a

--- ide/src/org/lgna/story/JointedModel.java
+++ ide/src/org/lgna/story/JointedModel.java
@@ -65,6 +65,7 @@ public abstract class JointedModel extends Model {
 	
 	@MethodTemplate()
 	public void say( String text, Say.Detail... details ) {
+		
 		edu.cmu.cs.dennisc.scenegraph.graphics.Bubble bubble = new edu.cmu.cs.dennisc.scenegraph.graphics.SpeechBubble();
 		bubble.text.setValue(text);
 		initializeBubble(bubble, details);
@@ -79,6 +80,13 @@ public abstract class JointedModel extends Model {
 		this.getImplementation().displayBubble( bubble, Duration.getValue(details));
 	}
 	
+	
+	public void showVisualization() {
+		this.getImplementation().showVisualization();
+	}
+	public void hideVisualization() {
+		this.getImplementation().hideVisualization();
+	}
 	
 	//TODO: Get this to work
 //	@MethodTemplate()
