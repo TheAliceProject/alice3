90942ab6ac2c356296f1380d76ea3ede7de742bb
Dennis Cosgrove <dennisc@cs.cmu.edu>
2011-12-22 13:07:52 -0600

    deleted old other directory of old projects
    Conflicts:
    
    	ide/.gitignore
    
    
    Former-commit-id: 96730d23949bfaf4b474c8a03736040af293a562

--- other/x_classpath/src/edu/cmu/cs/dennisc/lang/ClassPathUtilities.java
+++
@@ -1,8 +0,0 @@
-package edu.cmu.cs.dennisc.lang;
-
-public class ClassPathUtilities {
-	public static String[] getClassPath() {
-		String classPath = System.getProperty( "java.class.path" );
-		return classPath.split( System.getProperty( "path.separator" ) );
-	}
-}
