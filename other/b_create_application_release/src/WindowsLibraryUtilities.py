169cf7a1715466dd3f067306d6fa1386541e914e
Dennis Cosgrove <dennisc@cs.cmu.edu>
2011-12-22 13:07:52 -0600

    deleted old other directory of old projects
    Former-commit-id: 6c248b64a28f2a08f9c3845f7f649778acdc6a6a

--- other/b_create_application_release/src/WindowsLibraryUtilities.py
+++
@@ -1,6 +0,0 @@
-
-__author__="Administrator"
-__date__ ="$Jun 23, 2009 1:05:29 PM$"
-
-if __name__ == "__main__":
-    print "Hello";
--- other/x_issue/src/test/MyProgressPane.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-class MyProgressPane extends edu.cmu.cs.dennisc.issue.ProgressPane {
-	@Override
-	protected edu.cmu.cs.dennisc.issue.UploadWorker createUploadWorker() {
-		return new MyUploadWorker( MyProgressPane.this );
-	}
-}
-
