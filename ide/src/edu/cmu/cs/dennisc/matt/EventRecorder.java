7a06a0539f8c0ed906488ff5b5b11e51538a432b
Matt May <may.matt08@gmail.com>
2012-01-30 15:24:16 -0600

    merged with dennis
    Signed-off-by: Matt May <may.matt08@gmail.com>
    
    Former-commit-id: 248eb5be7820afbd2eeeac4999030d210630e9bc

--- ide/src/edu/cmu/cs/dennisc/matt/EventRecorder.java
+++ ide/src/edu/cmu/cs/dennisc/matt/EventRecorder.java
@@ -14,28 +14,28 @@ public class EventRecorder {
 	private static EventRecorder instance = new EventRecorder();
 	private Map<Long, AbstractEvent> eventMap = Collections.newHashMap();
 	private List<Long> remainingEvents = Collections.newArrayList();
-	
-	private EventRecorder(){
-		//make private
+
+	private EventRecorder() {
+		// make private
 	}
-	
-	public static EventRecorder getSingleton(){
+
+	public static EventRecorder getSingleton() {
 		return instance;
 	}
-	
-	public void recordEvent(AbstractEvent e){
+
+	public void recordEvent(AbstractEvent e) {
 		if (e instanceof SceneActivationEvent) {
 			startTime = System.currentTimeMillis();
 			eventMap.put(startTime - startTime, e);
-		}else if(startTime != null){
+		} else if (startTime != null) {
 			eventMap.put(System.currentTimeMillis() - startTime, e);
-		}else{
+		} else {
 			System.out.println("WARNING EVENTS NOT BEING PROPERLY RECORDED");
 		}
 	}
-	
-	public AbstractEvent eventToFire(Long time){
-		if(remainingEvents.get(0) < time){
+
+	public AbstractEvent eventToFire(Long time) {
+		if (remainingEvents.get(0) < time) {
 			return eventMap.get(remainingEvents.remove(0));
 		}
 		return null;
