8843b2f26abaa7890ae27ba696cb9270f7295ccd
Dennis Cosgrove <dennisc@cs.cmu.edu>
2012-02-28 19:18:43 -0500

    fix resize and resizeWidth/Height/Depth
    Former-commit-id: 8dde288fc96457ca00a7146d9ee02148ab3b3044

--- ide/src/org/lgna/story/implementation/ModelImp.java
+++ ide/src/org/lgna/story/implementation/ModelImp.java
@@ -310,19 +310,23 @@ public abstract class ModelImp extends TransformableImp implements org.alice.int
 		}
 	}
 
+	private void animateScale( edu.cmu.cs.dennisc.math.Dimension3 scale, double duration, edu.cmu.cs.dennisc.animation.Style style ) {
+		edu.cmu.cs.dennisc.math.Dimension3 prevScale = this.getScale();
+		this.animateSetScale( new edu.cmu.cs.dennisc.math.Dimension3( prevScale.x*scale.x, prevScale.y*scale.y, prevScale.z*scale.z ), duration, style );
+	}
+	
 	public void animateResize( double factor, double duration, edu.cmu.cs.dennisc.animation.Style style ) {
-		edu.cmu.cs.dennisc.math.Dimension3 scale = this.getScale();
-		this.animateSetScale( new edu.cmu.cs.dennisc.math.Dimension3( scale.x*factor, scale.y*factor, scale.z*factor ), duration, style );
+		this.animateScale( new edu.cmu.cs.dennisc.math.Dimension3( factor, factor, factor ), duration, style );
 	}
 	
 	public void animateResizeWidth( double factor, boolean isVolumePreserved, double duration, edu.cmu.cs.dennisc.animation.Style style ) {
-		this.animateSetScale( Dimension.LEFT_TO_RIGHT.getResizeAxis( factor, isVolumePreserved ), duration, style );
+		this.animateScale( Dimension.LEFT_TO_RIGHT.getResizeAxis( factor, isVolumePreserved ), duration, style );
 	}
 	public void animateResizeHeight( double factor, boolean isVolumePreserved, double duration, edu.cmu.cs.dennisc.animation.Style style ) {
-		this.animateSetScale( Dimension.TOP_TO_BOTTOM.getResizeAxis( factor, isVolumePreserved ), duration, style );
+		this.animateScale( Dimension.TOP_TO_BOTTOM.getResizeAxis( factor, isVolumePreserved ), duration, style );
 	}
 	public void animateResizeDepth( double factor, boolean isVolumePreserved, double duration, edu.cmu.cs.dennisc.animation.Style style ) {
-		this.animateSetScale( Dimension.FRONT_TO_BACK.getResizeAxis( factor, isVolumePreserved ), duration, style );
+		this.animateScale( Dimension.FRONT_TO_BACK.getResizeAxis( factor, isVolumePreserved ), duration, style );
 	}
 	
 	public void displayBubble(edu.cmu.cs.dennisc.scenegraph.graphics.Bubble bubble, Number duration) {
