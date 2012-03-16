a24dca01bad895eb9a121e1138f7bbb0b0a5e642
Dennis Cosgrove <dennisc@cs.cmu.edu>
2012-03-16 14:10:16 -0400

    remove events tab by default
    Former-commit-id: 0a518d4e2ae7c44750b9616102ddbbc6211214f9

--- ide/src/org/alice/ide/declarationseditor/DeclarationTabState.java
+++ ide/src/org/alice/ide/declarationseditor/DeclarationTabState.java
@@ -127,7 +127,7 @@ public class DeclarationTabState extends org.lgna.croquet.TabSelectionState< Dec
 				java.util.List< DeclarationComposite > items = edu.cmu.cs.dennisc.java.util.Collections.newLinkedList();
 				if( this.type != null ) {
 					items.add( DeclarationComposite.getInstance( this.type ) );
-					final boolean isInitializeEventListenersDesired = true;
+					final boolean isInitializeEventListenersDesired = false;
 					for( org.lgna.project.ast.UserMethod method : this.type.methods ) {
 						if( method.isPublicAccess() || ( isInitializeEventListenersDesired && StageIDE.INITIALIZE_EVENT_LISTENERS_METHOD_NAME.equals( method.getName() ) ) ) {
 							if( method.getManagementLevel() == org.lgna.project.ast.ManagementLevel.NONE ) {
