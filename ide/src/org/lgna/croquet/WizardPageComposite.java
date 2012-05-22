37450a428ca83e0366d918e0a35cc84d6b6f8466
Dennis Cosgrove <dennisc@totoro.seas.wustl.edu>
2012-05-22 16:42:31 -0500

    revalidate
    Former-commit-id: 4c7933357948d63251b021acfa8bfe909ed345cb

--- ide/src/org/lgna/croquet/WizardPageComposite.java
+++ ide/src/org/lgna/croquet/WizardPageComposite.java
@@ -50,4 +50,9 @@ public abstract class WizardPageComposite< V extends org.lgna.croquet.components
 	public WizardPageComposite( java.util.UUID id ) {
 		super( id );
 	}
+	@Override
+	public void handlePreActivation() {
+		super.handlePreActivation();
+		this.getView().revalidateAndRepaint();
+	}
 }
