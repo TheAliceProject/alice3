3f071b845721566b05419e460091d9cc7e7a846d
Kyle J. Harms <harmsk@seas.wustl.edu>
2012-06-06 10:11:50 -0500

    we need to hide, not that this works currently any way...
    references #280: Prepare for master merge
    
    Former-commit-id: 0409ae03a8d95e7fba78b84e6e2711a7741afd4b

--- ide/src/org/lgna/cheshire/test/IsShowingHackTutorialState.java
+++ ide/src/org/lgna/cheshire/test/IsShowingHackTutorialState.java
@@ -23,7 +23,7 @@ public class IsShowingHackTutorialState extends BooleanState {
 							org.alice.ide.croquet.models.ui.debug.ActiveTransactionHistoryComposite.getInstance().getBooleanState().setValue( true );
 							org.alice.ide.IDE.getActiveInstance().getSimplePresentation().showStencilsPresentation();
 						} else {
-							org.alice.ide.IDE.getActiveInstance().getSimplePresentation().showStencilsPresentation();
+							org.alice.ide.IDE.getActiveInstance().getSimplePresentation().hideStencilsPresentation();
 						}
 					}
 				});
