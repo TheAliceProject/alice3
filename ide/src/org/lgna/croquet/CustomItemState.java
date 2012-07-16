add9d5b912e848d51b5e59d4ea25185aeef271e4
Dennis Cosgrove <dennisc@cs.cmu.edu>
2012-07-16 18:41:55 -0400

    custom item state
    Former-commit-id: 411813b2a6bb8a5455258ead0b0ab8348c44e24e

--- ide/src/org/lgna/croquet/CustomItemState.java
+++ ide/src/org/lgna/croquet/CustomItemState.java
@@ -108,4 +108,10 @@ public abstract class CustomItemState< T > extends ItemState< T > {
 	@Override
 	protected void localize() {
 	}
+	@Override
+	public org.lgna.croquet.history.Transaction addGeneratedStateChangeTransaction(org.lgna.croquet.history.TransactionHistory history, T prevValue, T nextValue) {
+		org.lgna.croquet.history.Transaction rv = super.addGeneratedStateChangeTransaction( history, prevValue, nextValue );
+		org.lgna.croquet.history.PopupPrepStep.createAndAddToTransaction( rv, this.getCascadeRoot().getPopupPrepModel(), org.lgna.croquet.triggers.ActionEventTrigger.createGeneratorInstance() );
+		return rv;
+	}
 }
