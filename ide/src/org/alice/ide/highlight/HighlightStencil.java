321f663aa5251c44612ea524196da885985a7ed6
Dennis Cosgrove <dennisc@cs.cmu.edu>
2012-07-23 14:40:33 -0400

    scroll renderer
    Former-commit-id: 0e1a44e1d63f72edde1ff341ec4482626922eb00

--- ide/src/org/alice/ide/highlight/HighlightStencil.java
+++ ide/src/org/alice/ide/highlight/HighlightStencil.java
@@ -67,10 +67,8 @@ public class HighlightStencil extends org.lgna.croquet.components.LayerStencil {
 	}
 	private static final java.awt.Paint stencilPaint = createStencilPaint();
 
-	private org.lgna.croquet.resolvers.RuntimeResolver< org.lgna.croquet.components.TrackableShape > trackableShapeResolver;
+	private final org.lgna.cheshire.simple.ScrollRenderer scrollRenderer = new org.lgna.cheshire.simple.SimpleScrollRenderer();
 	private final org.lgna.stencil.Note note = new org.lgna.stencil.Note();
-
-	
 	public HighlightStencil( org.lgna.croquet.components.AbstractWindow<?> window, Integer layerId ) {
 		super( window, layerId );
 		this.note.setActive( true );
@@ -91,17 +89,6 @@ public class HighlightStencil extends org.lgna.croquet.components.LayerStencil {
 		};
 	}
 	
-//	private static final java.awt.Paint HIGHLIGHT_WITH_PATH_PAINT = new java.awt.Color(255, 255, 0, 23);
-//	private static final int HOLE_BEVEL_THICKNESS = 2;
-//	private static final java.awt.Stroke[] HIGHLIGHT_STROKES;
-//	static {
-//		final int N = 8;
-//		HIGHLIGHT_STROKES = new java.awt.Stroke[N];
-//		for (int i = 0; i < N; i++) {
-//			HIGHLIGHT_STROKES[i] = new java.awt.BasicStroke((i + 1) * 5.0f, java.awt.BasicStroke.CAP_ROUND, java.awt.BasicStroke.JOIN_ROUND);
-//		}
-//	};
-
 	@Override
 	protected void paintComponentPrologue( java.awt.Graphics2D g2 ) {
 		java.awt.Shape prevClip = g2.getClip();
@@ -109,46 +96,16 @@ public class HighlightStencil extends org.lgna.croquet.components.LayerStencil {
 		java.awt.Stroke prevStroke = g2.getStroke();
 
 		java.awt.Shape shape = prevClip;
-		java.awt.Shape visibleShape = null;
-		if( trackableShapeResolver != null ) {
-			org.lgna.croquet.components.TrackableShape trackableShape = this.trackableShapeResolver.getResolved();
-			if( trackableShape != null ) {
-				visibleShape = trackableShape.getVisibleShape( this, INSETS );
+		java.awt.geom.Area area = new java.awt.geom.Area( shape );
+		for( org.lgna.cheshire.simple.Feature feature : note.getFeatures() ) {
+			java.awt.geom.Area featureAreaToSubtract = feature.getAreaToSubstractForPaint( HighlightStencil.this );
+			if( featureAreaToSubtract != null ) {
+				area.subtract( featureAreaToSubtract );
+				shape = area;
 			}
-		}
-		if( visibleShape != null ) {
-			java.awt.geom.Area area = new java.awt.geom.Area( shape );
-			area.subtract( new java.awt.geom.Area( visibleShape ) );
-			shape = area;
 		}
 		g2.setPaint( stencilPaint );
 		g2.fill( shape );
-		
-//		if( visibleShape != null ) {
-//			g2.setClip(shape);
-//			java.awt.Paint paint = HIGHLIGHT_WITH_PATH_PAINT;
-//			g2.setPaint(paint);
-//			for (java.awt.Stroke stroke : HIGHLIGHT_STROKES) {
-//				g2.setStroke(stroke);
-//				g2.draw(visibleShape);
-//			}
-//			g2.setClip(prevClip);
-//		}
-//		
-//		if (visibleShape instanceof java.awt.Rectangle) {
-//			java.awt.Rectangle rect = (java.awt.Rectangle) visibleShape;
-//		
-//			int x0 = rect.x;
-//			int y0 = rect.y;
-//			int x1 = rect.x + rect.width - HOLE_BEVEL_THICKNESS;
-//			int y1 = rect.y + rect.height - HOLE_BEVEL_THICKNESS;
-//			g2.setPaint(java.awt.Color.DARK_GRAY);
-//			g2.fillRect(x0, y0, HOLE_BEVEL_THICKNESS, rect.height);
-//			g2.fillRect(x0, y0, rect.width, HOLE_BEVEL_THICKNESS);
-//			g2.setPaint(java.awt.Color.WHITE);
-//			g2.fillRect(x1, y0, HOLE_BEVEL_THICKNESS, rect.height);
-//			g2.fillRect(x0, y1, rect.width, HOLE_BEVEL_THICKNESS);
-//		}
 
 		g2.setStroke( prevStroke );
 		g2.setPaint( prevPaint );
@@ -160,61 +117,39 @@ public class HighlightStencil extends org.lgna.croquet.components.LayerStencil {
 				feature.paint( g2, HighlightStencil.this, note );
 			}
 		}
-//		Page page = getCurrentPage();
-//		if( this.isVisible() ) {
-//			if( page != null ) {
-//				for( Note note : page.getNotes() ) {
-//					if( note.isActive() ) {
-//						for( Feature feature : note.getFeatures() ) {
-//							feature.paint( g2, SimpleStencil.this, note );
-//						}
-//					}
-//				}
-//			}
-//		}
 	}
 
 	@Override
 	protected void paintEpilogue( java.awt.Graphics2D g2 ) {
-//		Page page = getCurrentPage();
-//		if( this.isVisible() ) {
-//			if( page != null ) {
-//				for( Note note : page.getNotes() ) {
-					if( note.isActive() ) {
-						for( org.lgna.cheshire.simple.Feature feature : note.getFeatures() ) {
-							org.lgna.croquet.components.TrackableShape trackableShape = feature.getTrackableShape();
-							if( trackableShape != null ) {
-								if( trackableShape.isInView() ) {
-									//pass
-								} else {
-//									if( scrollRenderer != null ) {
-//										java.awt.Shape repaintShape = scrollRenderer.renderScrollIndicators( g2, SimpleStencil.this, trackableShape );
-//										if( repaintShape != null ) {
-//											//todo: repaint?
-//										}
-//									}
-								}
+		if( note.isActive() ) {
+			for( org.lgna.cheshire.simple.Feature feature : note.getFeatures() ) {
+				org.lgna.croquet.components.TrackableShape trackableShape = feature.getTrackableShape();
+				if( trackableShape != null ) {
+					if( trackableShape.isInView() ) {
+						//pass
+					} else {
+						if( scrollRenderer != null ) {
+							java.awt.Shape repaintShape = scrollRenderer.renderScrollIndicators( g2, HighlightStencil.this, trackableShape );
+							if( repaintShape != null ) {
+								//todo: repaint?
 							}
 						}
-//					}
-//				}
-//			}
+					}
+				}
+			}
 		}
 	}
-	
-	public void show( org.lgna.croquet.resolvers.RuntimeResolver< org.lgna.croquet.components.TrackableShape > trackableShapeResolver, String noteText ) {
-		this.trackableShapeResolver = trackableShapeResolver;
-		this.note.removeAllFeatures();
-		this.note.addFeature( new org.lgna.cheshire.simple.stencil.features.Hole( trackableShapeResolver, org.lgna.cheshire.simple.Feature.ConnectionPreference.NORTH_SOUTH ) );
-		this.note.setText( noteText );
-		this.note.reset();
-		this.setStencilShowing( true );
-	}
-	
 	@Override
 	protected void processMouseEvent( java.awt.event.MouseEvent e ) {
 		if( e.getID() == java.awt.event.MouseEvent.MOUSE_PRESSED ) {
 			this.setStencilShowing( false );
 		}
+	}
+	public void show( org.lgna.croquet.resolvers.RuntimeResolver< org.lgna.croquet.components.TrackableShape > trackableShapeResolver, String noteText ) {
+		this.note.removeAllFeatures();
+		this.note.addFeature( new org.lgna.cheshire.simple.stencil.features.Hole( trackableShapeResolver, org.lgna.cheshire.simple.Feature.ConnectionPreference.NORTH_SOUTH ) );
+		this.note.setText( noteText );
+		this.note.reset();
+		this.setStencilShowing( true );
 	}
 }
