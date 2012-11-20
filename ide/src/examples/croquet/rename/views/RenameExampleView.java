9843dbbdd16586c0b0797e309d333382f0c874ec
Dennis Cosgrove <dennisc@cs.cmu.edu>
2012-11-20 17:51:49 -0600

    refactor
    Former-commit-id: 031308291a8aba2d08bd013de043376ed83849c5

--- ide/src/examples/croquet/rename/views/RenameExampleView.java
+++ ide/src/examples/croquet/rename/views/RenameExampleView.java
@@ -40,7 +40,7 @@
  * THE USE OF OR OTHER DEALINGS WITH THE SOFTWARE, EVEN IF ADVISED OF THE 
  * POSSIBILITY OF SUCH DAMAGE.
  */
-package examples.croquet.stringstate.views;
+package examples.croquet.rename.views;
 
 /**
  * @author Dennis Cosgrove
