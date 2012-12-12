f415340d4588a83814ddf7a45af224b03257a685
Dennis Cosgrove <dennisc@cs.cmu.edu>
2012-12-12 17:31:45 -0500

    remove search tab
    Former-commit-id: e1080a6bb5723a14adf69ec78b789f612f4fa100

--- ide/src/org/alice/ide/members/MembersComposite.java
+++ ide/src/org/alice/ide/members/MembersComposite.java
@@ -57,7 +57,6 @@ public class MembersComposite extends org.lgna.croquet.SimpleComposite<org.alice
 
 	private final org.alice.ide.member.ProcedureTabComposite procedureTabComposite = new org.alice.ide.member.ProcedureTabComposite();
 	private final org.alice.ide.member.FunctionTabComposite functionTabComposite = new org.alice.ide.member.FunctionTabComposite();
-	private final org.alice.ide.member.SearchTabComposite searchTabComposite = new org.alice.ide.member.SearchTabComposite();
 	private final org.alice.ide.member.ControlFlowTabComposite controlStructureTabComposite;
 	private final org.lgna.croquet.TabSelectionState<org.alice.ide.member.MemberOrControlFlowTabComposite> tabState;
 
@@ -66,10 +65,10 @@ public class MembersComposite extends org.lgna.croquet.SimpleComposite<org.alice
 		org.alice.ide.member.MemberOrControlFlowTabComposite[] tabComposites;
 		if( org.alice.ide.croquet.models.ui.preferences.IsAlwaysShowingBlocksState.getInstance().getValue() ) {
 			this.controlStructureTabComposite = null;
-			tabComposites = new org.alice.ide.member.MemberOrControlFlowTabComposite[] { this.procedureTabComposite, this.functionTabComposite, this.searchTabComposite };
+			tabComposites = new org.alice.ide.member.MemberOrControlFlowTabComposite[] { this.procedureTabComposite, this.functionTabComposite };
 		} else {
 			this.controlStructureTabComposite = new org.alice.ide.member.ControlFlowTabComposite();
-			tabComposites = new org.alice.ide.member.MemberOrControlFlowTabComposite[] { this.procedureTabComposite, this.functionTabComposite, this.searchTabComposite, this.controlStructureTabComposite };
+			tabComposites = new org.alice.ide.member.MemberOrControlFlowTabComposite[] { this.procedureTabComposite, this.functionTabComposite, this.controlStructureTabComposite };
 		}
 		this.tabState = this.createTabSelectionState( this.createKey( "tabState" ), org.alice.ide.member.MemberOrControlFlowTabComposite.class, 0, tabComposites );
 	}
