173ed195ed02fc76b76fd15b200b3272311263e1
Dennis Cosgrove <dennisc@cs.cmu.edu>
2012-12-19 16:04:02 -0600

    data.fire
    Former-commit-id: 2a5ed2291f2afde8efdb57250413b0a6610e3376

--- ide/src/org/lgna/croquet/data/AbstractMutableListData.java
+++ ide/src/org/lgna/croquet/data/AbstractMutableListData.java
@@ -62,10 +62,28 @@ public abstract class AbstractMutableListData<T> extends ListData<T> {
 		this.listeners.remove( listener );
 	}
 
-	protected final void fireContentsChanged() {
-		javax.swing.event.ListDataEvent e = new javax.swing.event.ListDataEvent( this, javax.swing.event.ListDataEvent.CONTENTS_CHANGED, 0, this.getItemCount() - 1 );
+	private void fireItervalAdded( int indexA, int indexB ) {
+		javax.swing.event.ListDataEvent e = new javax.swing.event.ListDataEvent( this, javax.swing.event.ListDataEvent.INTERVAL_ADDED, indexA, indexB );
 		for( javax.swing.event.ListDataListener listener : this.listeners ) {
-			listener.contentsChanged( e );
+			listener.intervalAdded( e );
 		}
+	}
+
+	private void fireItervalRemoved( int indexA, int indexB ) {
+		javax.swing.event.ListDataEvent e = new javax.swing.event.ListDataEvent( this, javax.swing.event.ListDataEvent.INTERVAL_REMOVED, indexA, indexB );
+		for( javax.swing.event.ListDataListener listener : this.listeners ) {
+			listener.intervalRemoved( e );
+		}
+	}
+
+	private void fireContentsChanged( int indexA, int indexB ) {
+		javax.swing.event.ListDataEvent e = new javax.swing.event.ListDataEvent( this, javax.swing.event.ListDataEvent.CONTENTS_CHANGED, indexA, indexB );
+		for( javax.swing.event.ListDataListener listener : this.listeners ) {
+			listener.intervalRemoved( e );
+		}
+	}
+
+	protected final void fireContentsChanged() {
+		this.fireContentsChanged( 0, this.getItemCount() - 1 );
 	}
 }
