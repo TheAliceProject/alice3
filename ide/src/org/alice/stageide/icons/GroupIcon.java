616c947f5c1a3d65d8dcbb7857f779cbdd54fde0
Dennis Cosgrove <dennisc@cs.cmu.edu>
2013-01-16 00:04:23 -0500

    group icon layout
    Former-commit-id: 5a1044a00a589aea06cdbd6198bacb0ad1a1f866

--- ide/src/org/alice/stageide/icons/GroupIcon.java
+++ ide/src/org/alice/stageide/icons/GroupIcon.java
@@ -53,7 +53,7 @@ public class GroupIcon extends ShapeIcon {
 
 	static {
 		xs = new double[ N ];
-		double xDelta = 0.2;
+		double xDelta = 0.15;
 		double x = 0.0;
 		for( int i = 0; i < N; i++ ) {
 			xs[ i ] = x;
@@ -148,23 +148,33 @@ public class GroupIcon extends ShapeIcon {
 
 			g2.setTransform( t );
 
-			for( int i : this.drawOrder ) {
-				javax.swing.Icon icon = this.icons[ i ];
-				int x = (int)( xs[ i ] * width );
-				int y = (int)( ys[ i ] * height );
+			if( this.drawOrder.length > 0 ) {
+				int totalAvailableArea = width * height;
+				int totalIconArea = 0;
+				for( int i : this.drawOrder ) {
+					javax.swing.Icon icon = this.icons[ i ];
+					int iconIArea = icon.getIconWidth() * icon.getIconHeight();
+					totalIconArea += iconIArea;
+				}
+				//edu.cmu.cs.dennisc.java.util.logging.Logger.outln( totalAvailableArea, totalIconArea );
+				double scale = 0.5;
+				for( int i : this.drawOrder ) {
+					javax.swing.Icon icon = this.icons[ i ];
+					int x = (int)( xs[ i ] * width );
+					int y = (int)( ys[ i ] * height );
 
-				if( icon instanceof javax.swing.ImageIcon ) {
-					javax.swing.ImageIcon imageIcon = (javax.swing.ImageIcon)icon;
-					int imageWidth = imageIcon.getIconWidth();
-					int imageHeight = imageIcon.getIconHeight();
+					if( icon instanceof javax.swing.ImageIcon ) {
+						javax.swing.ImageIcon imageIcon = (javax.swing.ImageIcon)icon;
+						int imageWidth = imageIcon.getIconWidth();
+						int imageHeight = imageIcon.getIconHeight();
 
-					double scale = Math.min( ( width * 0.25 ) / imageWidth, ( height * 0.8 ) / imageHeight );
-					int dstWidth = (int)( imageWidth * scale );
-					int dstHeight = (int)( imageHeight * scale );
+						int dstWidth = (int)( imageWidth * scale );
+						int dstHeight = (int)( imageHeight * scale );
 
-					g2.drawImage( imageIcon.getImage(), x, y, ( x + dstWidth ) - 1, ( y + (int)dstHeight ) - 1, 0, 0, imageWidth - 1, imageHeight - 1, c );
-				} else {
-					icon.paintIcon( c, g2, x, y );
+						g2.drawImage( imageIcon.getImage(), x, y, ( x + dstWidth ) - 1, ( y + (int)dstHeight ) - 1, 0, 0, imageWidth - 1, imageHeight - 1, c );
+					} else {
+						icon.paintIcon( c, g2, x, y );
+					}
 				}
 			}
 
