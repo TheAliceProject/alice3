fdc350b3d6f6f0768e2eee1512a741696caa2e03
Dennis Cosgrove <dennisc@cs.cmu.edu>
2013-09-24 23:31:51 -0400

    save
    Former-commit-id: 404069f8a9be8e1e6d7ad25175ee562fe4aeb27c

--- ide/src/org/alice/imageeditor/croquet/SaveOperation.java
+++ ide/src/org/alice/imageeditor/croquet/SaveOperation.java
@@ -101,6 +101,9 @@ public final class SaveOperation extends org.lgna.croquet.SingleThreadIteratingO
 
 	@Override
 	protected void handleSuccessfulCompletionOfSubModels( org.lgna.croquet.history.CompletionStep<?> step, java.util.List<org.lgna.croquet.history.Step<?>> subSteps ) {
-		edu.cmu.cs.dennisc.java.util.logging.Logger.outln( "todo: save" );
+		if( this.file != null ) {
+			edu.cmu.cs.dennisc.image.ImageUtilities.write( this.file, owner.getView().render() );
+			this.getOwner().getBooleanState().setValueTransactionlessly( false );
+		}
 	}
 }
