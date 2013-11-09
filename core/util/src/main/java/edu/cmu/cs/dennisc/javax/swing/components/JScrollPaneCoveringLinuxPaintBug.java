6145c035569cb02700566a3654e095ee5b9a1d7a
Dennis Cosgrove <dennisc@cs.cmu.edu>
2013-11-08 18:01:13 -0600

    smaller footprint scrollbar
    Former-commit-id: 7dfb6e3ae3cf4812c07707b6d5b3890d258baaac

--- core/util/src/main/java/edu/cmu/cs/dennisc/javax/swing/components/JScrollPaneCoveringLinuxPaintBug.java
+++ core/util/src/main/java/edu/cmu/cs/dennisc/javax/swing/components/JScrollPaneCoveringLinuxPaintBug.java
@@ -46,9 +46,175 @@ package edu.cmu.cs.dennisc.javax.swing.components;
  * @author Dennis Cosgrove
  */
 public class JScrollPaneCoveringLinuxPaintBug extends javax.swing.JScrollPane {
+	private static class SmallerFootprintScrollBarUI extends javax.swing.plaf.ScrollBarUI {
+
+		private static final int SIZE = 9;
+		private static final float CENTER = SIZE * 0.5f;
+		private static final java.awt.Stroke STROKE = new java.awt.BasicStroke( SIZE - 2, java.awt.BasicStroke.CAP_ROUND, java.awt.BasicStroke.JOIN_MITER );
+
+		private static final java.awt.Color BASE_COLOR = edu.cmu.cs.dennisc.java.awt.ColorUtilities.createGray( 140 );
+		private static final java.awt.Color ROLL_OVER_COLOR = edu.cmu.cs.dennisc.java.awt.ColorUtilities.createGray( 100 );
+		private static final java.awt.Color PRESSED_COLOR = new java.awt.Color( 100, 140, 255 );
+
+		public static javax.swing.plaf.ScrollBarUI createUI( javax.swing.JScrollBar scrollBar ) {
+			return new SmallerFootprintScrollBarUI( scrollBar );
+		}
+
+		private SmallerFootprintScrollBarUI( javax.swing.JScrollBar scrollBar ) {
+			this.scrollBar = scrollBar;
+		}
+
+		@Override
+		public void installUI( javax.swing.JComponent c ) {
+			super.installUI( c );
+			c.addMouseListener( this.mouseListener );
+			c.addMouseMotionListener( this.mouseMotionListener );
+			this.scrollBar.getModel().addChangeListener( this.changeListener );
+		}
+
+		@Override
+		public void uninstallUI( javax.swing.JComponent c ) {
+			this.scrollBar.getModel().removeChangeListener( this.changeListener );
+			c.removeMouseMotionListener( this.mouseMotionListener );
+			c.removeMouseListener( this.mouseListener );
+			super.uninstallUI( c );
+		}
+
+		@Override
+		public void paint( java.awt.Graphics g, javax.swing.JComponent c ) {
+			super.paint( g, c );
+			javax.swing.JScrollBar scrollBar = (javax.swing.JScrollBar)c;
+			int orientation = scrollBar.getOrientation();
+
+			java.awt.Graphics2D g2 = (java.awt.Graphics2D)g;
+
+			edu.cmu.cs.dennisc.java.awt.GraphicsContext gc = new edu.cmu.cs.dennisc.java.awt.GraphicsContext();
+			gc.pushAll( g2 );
+			gc.pushPaint();
+			gc.pushStroke();
+			gc.pushAndSetAntialiasing( true );
+
+			java.awt.Paint paint;
+			if( isPressed() ) {
+				paint = PRESSED_COLOR;
+			} else {
+				paint = isRollover() ? ROLL_OVER_COLOR : BASE_COLOR;
+			}
+			g2.setPaint( paint );
+			g2.setStroke( STROKE );
+
+			int indent = 4;
+
+			int s = orientation == javax.swing.SwingConstants.VERTICAL ? c.getHeight() : c.getWidth();
+			s -= 1;
+			s -= indent;
+			s -= indent;
+
+			float min = scrollBar.getMinimum();
+			float max = scrollBar.getMaximum();
+			float extent = scrollBar.getVisibleAmount();
+			float range = max - min;
+
+			int value = scrollBar.getValue();
+			int position = value; //todo
+			int length = (int)( s * ( extent / range ) );
+
+			java.awt.geom.GeneralPath path = new java.awt.geom.GeneralPath();
+			if( orientation == javax.swing.SwingConstants.VERTICAL ) {
+				path.moveTo( CENTER, position + indent );
+				path.lineTo( CENTER, position + indent + length );
+			} else {
+				path.moveTo( position + indent, CENTER );
+				path.lineTo( position + indent + length, CENTER );
+			}
+			g2.draw( path );
+
+			gc.popAll();
+
+			//g.setColor( java.awt.Color.RED );
+			//g.drawRect( 0, 0, c.getWidth() - 1, c.getHeight() - 1 );
+		}
+
+		@Override
+		public java.awt.Dimension getPreferredSize( javax.swing.JComponent c ) {
+			return new java.awt.Dimension( SIZE, SIZE );
+		}
+
+		private boolean isRollover() {
+			return this.isRollover;
+		}
+
+		private void setRollover( boolean isRollover ) {
+			this.isRollover = isRollover;
+			this.scrollBar.repaint();
+
+		}
+
+		private boolean isPressed() {
+			return this.isPressed;
+		}
+
+		private void setPressed( boolean isPressed ) {
+			this.isPressed = isPressed;
+			this.scrollBar.repaint();
+
+		}
+
+		private final javax.swing.JScrollBar scrollBar;
+		private final java.awt.event.MouseListener mouseListener = new java.awt.event.MouseListener() {
+			public void mouseEntered( java.awt.event.MouseEvent e ) {
+				setRollover( true );
+			}
+
+			public void mouseExited( java.awt.event.MouseEvent e ) {
+				setRollover( false );
+			}
+
+			public void mousePressed( java.awt.event.MouseEvent e ) {
+				setPressed( true );
+			}
+
+			public void mouseReleased( java.awt.event.MouseEvent e ) {
+				setPressed( false );
+			}
+
+			public void mouseClicked( java.awt.event.MouseEvent e ) {
+			}
+		};
+
+		private final java.awt.event.MouseMotionListener mouseMotionListener = new java.awt.event.MouseMotionListener() {
+			public void mouseMoved( java.awt.event.MouseEvent e ) {
+			}
+
+			public void mouseDragged( java.awt.event.MouseEvent e ) {
+				//todo:
+			}
+		};
+
+		private final javax.swing.event.ChangeListener changeListener = new javax.swing.event.ChangeListener() {
+			public void stateChanged( javax.swing.event.ChangeEvent e ) {
+				scrollBar.repaint();
+			}
+		};
+
+		private boolean isRollover;
+		private boolean isPressed;
+	};
+
 	public JScrollPaneCoveringLinuxPaintBug() {
 		if( edu.cmu.cs.dennisc.java.lang.SystemUtilities.isLinux() ) {
 			this.getViewport().setScrollMode( javax.swing.JViewport.SIMPLE_SCROLL_MODE );
+		}
+		final boolean IS_SMALLER_FOOTPRINT_SCROLL_BAR_READY_FOR_PRIME_TIME = false;
+		if( IS_SMALLER_FOOTPRINT_SCROLL_BAR_READY_FOR_PRIME_TIME ) {
+			final java.awt.Color backgroundColor = edu.cmu.cs.dennisc.java.awt.ColorUtilities.createGray( 180 );
+			javax.swing.JScrollBar verticalScrollBar = this.getVerticalScrollBar();
+			verticalScrollBar.setUI( SmallerFootprintScrollBarUI.createUI( verticalScrollBar ) );
+			verticalScrollBar.setBackground( backgroundColor );
+
+			javax.swing.JScrollBar horizontalScrollBar = this.getHorizontalScrollBar();
+			horizontalScrollBar.setUI( SmallerFootprintScrollBarUI.createUI( horizontalScrollBar ) );
+			horizontalScrollBar.setBackground( backgroundColor );
 		}
 	}
 }
