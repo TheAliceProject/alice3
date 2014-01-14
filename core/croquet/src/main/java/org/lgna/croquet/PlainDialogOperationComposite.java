85750b49283c8adf998d9616100f2952835aa44f
Dennis Cosgrove <dennisc@cs.cmu.edu>
2014-01-14 11:17:34 -0600

    remove close operation
    Former-commit-id: 829e713b75aebbbc13b0c1226379d4ebe09ebecc

--- core/croquet/src/main/java/org/lgna/croquet/PlainDialogOperationComposite.java
+++ core/croquet/src/main/java/org/lgna/croquet/PlainDialogOperationComposite.java
@@ -46,48 +46,6 @@ package org.lgna.croquet;
  * @author Dennis Cosgrove
  */
 public abstract class PlainDialogOperationComposite<V extends org.lgna.croquet.views.CompositeView<?, ?>> extends AbstractDialogComposite<V> implements OperationOwningComposite<V> {
-	public static final class InternalCloseOperationResolver extends IndirectResolver<InternalCloseOperation, PlainDialogOperationComposite> {
-		private InternalCloseOperationResolver( PlainDialogOperationComposite indirect ) {
-			super( indirect );
-		}
-
-		public InternalCloseOperationResolver( edu.cmu.cs.dennisc.codec.BinaryDecoder binaryDecoder ) {
-			super( binaryDecoder );
-		}
-
-		@Override
-		protected InternalCloseOperation getDirect( PlainDialogOperationComposite indirect ) {
-			return indirect.getCloseOperation();
-		}
-	}
-
-	public static class InternalCloseOperation extends Operation {
-		private final PlainDialogOperationComposite plainDialogOperationComposite;
-
-		private InternalCloseOperation( PlainDialogOperationComposite plainDialogOperationComposite ) {
-			super( DIALOG_IMPLEMENTATION_GROUP, java.util.UUID.fromString( "2a116435-9536-4590-8294-c4050ea65a4e" ) );
-			assert plainDialogOperationComposite != null;
-			this.plainDialogOperationComposite = plainDialogOperationComposite;
-		}
-
-		public PlainDialogOperationComposite getPlainDialogOperationComposite() {
-			return this.plainDialogOperationComposite;
-		}
-
-		@Override
-		protected InternalCloseOperationResolver createResolver() {
-			return new InternalCloseOperationResolver( this.plainDialogOperationComposite );
-		}
-
-		@Override
-		protected final void perform( org.lgna.croquet.history.Transaction transaction, org.lgna.croquet.triggers.Trigger trigger ) {
-			org.lgna.croquet.history.CompletionStep<?> step = transaction.createAndSetCompletionStep( this, trigger );
-			step.finish();
-		}
-	}
-
-	private InternalCloseOperation closeOperation = new InternalCloseOperation( this );
-
 	private final OwnedByCompositeOperation launchOperation;
 
 	public PlainDialogOperationComposite( java.util.UUID migrationId, Group operationGroup, boolean isModal ) {
@@ -125,10 +83,6 @@ public abstract class PlainDialogOperationComposite<V extends org.lgna.croquet.v
 	@Override
 	protected void handlePostHideDialog( org.lgna.croquet.history.CompletionStep<?> step ) {
 		this.handlePostDeactivation();
-	}
-
-	public InternalCloseOperation getCloseOperation() {
-		return this.closeOperation;
 	}
 
 	@Override
