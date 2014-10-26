2e40852012be90cb0cc169ada4b856370439a3c9
Dennis Cosgrove <dennisc@cs.cmu.edu>
2014-10-26 01:21:57 -0500

    update to jogl 2.2
    Former-commit-id: 1e02b737744fbbfaf8eda8840190f72f7c359283

--- core/scenegraph/src/main/java/edu/cmu/cs/dennisc/lookingglass/opengl/GLEventAdapter.java
+++ core/scenegraph/src/main/java/edu/cmu/cs/dennisc/lookingglass/opengl/GLEventAdapter.java
@@ -1,43 +1,43 @@
 /*
  * Copyright (c) 2006-2010, Carnegie Mellon University. All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without 
+ *
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
- * 1. Redistributions of source code must retain the above copyright notice, 
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  *
- * 2. Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
  *
- * 3. Products derived from the software may not be called "Alice", nor may 
- *    "Alice" appear in their name, without prior written permission of 
+ * 3. Products derived from the software may not be called "Alice", nor may
+ *    "Alice" appear in their name, without prior written permission of
  *    Carnegie Mellon University.
  *
  * 4. All advertising materials mentioning features or use of this software must
- *    display the following acknowledgement: "This product includes software 
+ *    display the following acknowledgement: "This product includes software
  *    developed by Carnegie Mellon University"
  *
- * 5. The gallery of art assets and animations provided with this software is 
- *    contributed by Electronic Arts Inc. and may be used for personal, 
- *    non-commercial, and academic use only. Redistributions of any program 
+ * 5. The gallery of art assets and animations provided with this software is
+ *    contributed by Electronic Arts Inc. and may be used for personal,
+ *    non-commercial, and academic use only. Redistributions of any program
  *    source code that utilizes The Sims 2 Assets must also retain the copyright
- *    notice, list of conditions and the disclaimer contained in 
+ *    notice, list of conditions and the disclaimer contained in
  *    The Alice 3.0 Art Gallery License.
- * 
+ *
  * DISCLAIMER:
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.  
- * ANY AND ALL EXPRESS, STATUTORY OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,  FITNESS FOR A 
- * PARTICULAR PURPOSE, TITLE, AND NON-INFRINGEMENT ARE DISCLAIMED. IN NO EVENT 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+ * ANY AND ALL EXPRESS, STATUTORY OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,  FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, AND NON-INFRINGEMENT ARE DISCLAIMED. IN NO EVENT
  * SHALL THE AUTHORS, COPYRIGHT OWNERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR CONSEQUENTIAL DAMAGES 
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING FROM OR OTHERWISE RELATING TO 
- * THE USE OF OR OTHER DEALINGS WITH THE SOFTWARE, EVEN IF ADVISED OF THE 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING FROM OR OTHERWISE RELATING TO
+ * THE USE OF OR OTHER DEALINGS WITH THE SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
@@ -128,7 +128,7 @@ class GLEventAdapter implements javax.media.opengl.GLEventListener {
 	//	private void paintOverlay() {
 	//		edu.cmu.cs.dennisc.lookingglass.Overlay overlay = this.lookingGlass.getOverlay();
 	//		if( overlay != null ) {
-	//			
+	//
 	//			this.renderContext.gl.glMatrixMode( GL_PROJECTION );
 	//			this.renderContext.gl.glPushMatrix();
 	//			this.renderContext.gl.glLoadIdentity();
@@ -143,7 +143,7 @@ class GLEventAdapter implements javax.media.opengl.GLEventListener {
 	//			this.renderContext.setDiffuseColorTextureAdapter( null );
 	//			this.renderContext.setBumpTextureAdapter( null );
 	//
-	//			
+	//
 	//			try {
 	//				overlay.paint( this.lookingGlass );
 	//				this.renderContext.gl.glFlush();
@@ -257,15 +257,15 @@ class GLEventAdapter implements javax.media.opengl.GLEventListener {
 		//			edu.cmu.cs.dennisc.print.PrintUtilities.println( "createBufferedImageForUseAsColorBuffer: opengl is not initialized yet, so we will assume the GL_EXT_abgr extension is present." );
 		//			isClearedToCreateImage = true;
 		//		}
-		//		
-		//		
+		//
+		//
 		//		//todo: investigate
 		//		if( isClearedToCreateImage ) {
 		//			//pass
 		//		} else {
 		//			isClearedToCreateImage = true;
 		//		}
-		//		
+		//
 		//		if( isClearedToCreateImage ) {
 		//			//todo:
 		//			//int type = java.awt.image.BufferedImage.TYPE_3BYTE_ABGR;
@@ -304,7 +304,8 @@ class GLEventAdapter implements javax.media.opengl.GLEventListener {
 	}
 
 	public java.awt.image.BufferedImage getColorBufferWithTransparencyBasedOnDepthBuffer( java.awt.image.BufferedImage rv, java.nio.FloatBuffer depthBuffer, boolean[] atIsUpsideDown ) {
-		if( this.drawable.getContext() == javax.media.opengl.GLContext.getCurrent() ) {
+		javax.media.opengl.GLContext glCurrentContext = javax.media.opengl.GLContext.getCurrent();
+		if( ( glCurrentContext != null ) && ( glCurrentContext == this.drawable.getContext() ) ) {
 			this.renderContext.captureBuffers( rv, depthBuffer, atIsUpsideDown );
 		} else {
 			if( this.rvColorBuffer != null ) {
--- core/scenegraph/src/main/java/edu/cmu/cs/dennisc/lookingglass/opengl/GlDrawableUtilities.java
+++ core/scenegraph/src/main/java/edu/cmu/cs/dennisc/lookingglass/opengl/GlDrawableUtilities.java
@@ -1,43 +1,43 @@
 /**
  * Copyright (c) 2006-2012, Carnegie Mellon University. All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without 
+ *
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
- * 1. Redistributions of source code must retain the above copyright notice, 
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  *
- * 2. Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
  *
- * 3. Products derived from the software may not be called "Alice", nor may 
- *    "Alice" appear in their name, without prior written permission of 
+ * 3. Products derived from the software may not be called "Alice", nor may
+ *    "Alice" appear in their name, without prior written permission of
  *    Carnegie Mellon University.
  *
  * 4. All advertising materials mentioning features or use of this software must
- *    display the following acknowledgement: "This product includes software 
+ *    display the following acknowledgement: "This product includes software
  *    developed by Carnegie Mellon University"
  *
- * 5. The gallery of art assets and animations provided with this software is 
- *    contributed by Electronic Arts Inc. and may be used for personal, 
- *    non-commercial, and academic use only. Redistributions of any program 
+ * 5. The gallery of art assets and animations provided with this software is
+ *    contributed by Electronic Arts Inc. and may be used for personal,
+ *    non-commercial, and academic use only. Redistributions of any program
  *    source code that utilizes The Sims 2 Assets must also retain the copyright
- *    notice, list of conditions and the disclaimer contained in 
+ *    notice, list of conditions and the disclaimer contained in
  *    The Alice 3.0 Art Gallery License.
- * 
+ *
  * DISCLAIMER:
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.  
- * ANY AND ALL EXPRESS, STATUTORY OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,  FITNESS FOR A 
- * PARTICULAR PURPOSE, TITLE, AND NON-INFRINGEMENT ARE DISCLAIMED. IN NO EVENT 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+ * ANY AND ALL EXPRESS, STATUTORY OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,  FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, AND NON-INFRINGEMENT ARE DISCLAIMED. IN NO EVENT
  * SHALL THE AUTHORS, COPYRIGHT OWNERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR CONSEQUENTIAL DAMAGES 
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING FROM OR OTHERWISE RELATING TO 
- * THE USE OF OR OTHER DEALINGS WITH THE SOFTWARE, EVEN IF ADVISED OF THE 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING FROM OR OTHERWISE RELATING TO
+ * THE USE OF OR OTHER DEALINGS WITH THE SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
 package edu.cmu.cs.dennisc.lookingglass.opengl;
@@ -46,7 +46,7 @@ package edu.cmu.cs.dennisc.lookingglass.opengl;
  * @author Dennis Cosgrove
  */
 public class GlDrawableUtilities {
-	private static final java.util.Map<javax.media.opengl.GLPbuffer, java.awt.Dimension> mapPixelBufferToDimension;
+	private static final java.util.Map<javax.media.opengl.GLOffscreenAutoDrawable, java.awt.Dimension> mapPixelBufferToDimension;
 
 	private static boolean areEquivalentIgnoringMultisample( javax.media.opengl.GLCapabilitiesImmutable a, javax.media.opengl.GLCapabilitiesImmutable b ) {
 		if( a.getAccumAlphaBits() != b.getAccumAlphaBits() ) {
@@ -210,11 +210,11 @@ public class GlDrawableUtilities {
 	}
 
 	/* package-private */static javax.media.opengl.awt.GLCanvas createGLCanvas() {
-		return new javax.media.opengl.awt.GLCanvas( createGlCapabilities(), getPerhapsMultisampledGlCapabilitiesChooser(), null, null );
+		return new javax.media.opengl.awt.GLCanvas( createGlCapabilities(), getPerhapsMultisampledGlCapabilitiesChooser(), null );
 	}
 
 	/* package-private */static javax.media.opengl.awt.GLJPanel createGLJPanel() {
-		return new javax.media.opengl.awt.GLJPanel( createGlCapabilitiesForLightweightComponent(), getPerhapsMultisampledGlCapabilitiesChooser(), null );
+		return new javax.media.opengl.awt.GLJPanel( createGlCapabilitiesForLightweightComponent(), getPerhapsMultisampledGlCapabilitiesChooser() );
 	}
 
 	/* package-private */static boolean canCreateExternalGLDrawable() {
@@ -249,11 +249,11 @@ public class GlDrawableUtilities {
 		return glDrawableFactory.canCreateGLPbuffer( glDrawableFactory.getDefaultDevice(), glProfile );
 	}
 
-	/* package-private */static javax.media.opengl.GLPbuffer createGlPixelBuffer( javax.media.opengl.GLCapabilities glCapabilities, javax.media.opengl.GLCapabilitiesChooser glCapabilitiesChooser, int width, int height, javax.media.opengl.GLContext share ) {
+	/* package-private */static javax.media.opengl.GLOffscreenAutoDrawable createGlPixelBuffer( javax.media.opengl.GLCapabilities glCapabilities, javax.media.opengl.GLCapabilitiesChooser glCapabilitiesChooser, int width, int height, javax.media.opengl.GLContext share ) {
 		javax.media.opengl.GLProfile glProfile = javax.media.opengl.GLProfile.getDefault();
 		javax.media.opengl.GLDrawableFactory glDrawableFactory = javax.media.opengl.GLDrawableFactory.getFactory( glProfile );
 		if( glDrawableFactory.canCreateGLPbuffer( glDrawableFactory.getDefaultDevice(), glProfile ) ) {
-			javax.media.opengl.GLPbuffer buffer = glDrawableFactory.createGLPbuffer( glDrawableFactory.getDefaultDevice(), glCapabilities, glCapabilitiesChooser, width, height, share );
+			javax.media.opengl.GLOffscreenAutoDrawable buffer = glDrawableFactory.createOffscreenAutoDrawable( glDrawableFactory.getDefaultDevice(), glCapabilities, glCapabilitiesChooser, width, height );//, share );
 
 			// This is a work around for Linux users.
 			// Because of a bug in mesa (https://bugs.freedesktop.org/show_bug.cgi?id=24320) sometimes on Linux the method glXQueryDrawable() will
@@ -277,11 +277,11 @@ public class GlDrawableUtilities {
 
 	/* package-private */static int getGlDrawableWidth( javax.media.opengl.GLDrawable drawable ) {
 		// Bug in linux opengl, getWidth ALWAYS returns 0
-		int width = drawable.getWidth();
+		int width = drawable.getSurfaceWidth();
 		if( width == 0 ) {
 			if( mapPixelBufferToDimension != null ) {
-				if( drawable instanceof javax.media.opengl.GLPbuffer ) {
-					javax.media.opengl.GLPbuffer glPixelBuffer = (javax.media.opengl.GLPbuffer)drawable;
+				if( drawable instanceof javax.media.opengl.GLOffscreenAutoDrawable ) {
+					javax.media.opengl.GLOffscreenAutoDrawable glPixelBuffer = (javax.media.opengl.GLOffscreenAutoDrawable)drawable;
 					java.awt.Dimension size = mapPixelBufferToDimension.get( glPixelBuffer );
 					if( size != null ) {
 						width = size.width;
@@ -294,11 +294,11 @@ public class GlDrawableUtilities {
 
 	/* package-private */static int getGlDrawableHeight( javax.media.opengl.GLDrawable drawable ) {
 		// Bug in linux opengl, getHeight ALWAYS returns 0
-		int height = drawable.getHeight();
+		int height = drawable.getSurfaceHeight();
 		if( height == 0 ) {
 			if( mapPixelBufferToDimension != null ) {
-				if( drawable instanceof javax.media.opengl.GLPbuffer ) {
-					javax.media.opengl.GLPbuffer glPixelBuffer = (javax.media.opengl.GLPbuffer)drawable;
+				if( drawable instanceof javax.media.opengl.GLOffscreenAutoDrawable ) {
+					javax.media.opengl.GLOffscreenAutoDrawable glPixelBuffer = (javax.media.opengl.GLOffscreenAutoDrawable)drawable;
 					java.awt.Dimension size = mapPixelBufferToDimension.get( glPixelBuffer );
 					if( size != null ) {
 						height = size.height;
