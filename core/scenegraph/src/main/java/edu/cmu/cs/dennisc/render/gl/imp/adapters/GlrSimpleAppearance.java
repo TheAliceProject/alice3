36139239674d719fbabc0d4f580dffbcf0790487
Dennis Cosgrove <dennisc@cs.cmu.edu>
2014-12-30 14:48:26 -0600

    simple appearance
    Former-commit-id: ccec57eb9f79bebed90e41d15afc25dbec3f49c5

--- core/scenegraph/src/main/java/edu/cmu/cs/dennisc/render/gl/imp/adapters/GlrSimpleAppearance.java
+++ core/scenegraph/src/main/java/edu/cmu/cs/dennisc/render/gl/imp/adapters/GlrSimpleAppearance.java
@@ -15,59 +15,46 @@ import static javax.media.opengl.fixedfunc.GLLightingFunc.GL_SPECULAR;
 import edu.cmu.cs.dennisc.render.gl.imp.RenderContext;
 
 public class GlrSimpleAppearance<E extends edu.cmu.cs.dennisc.scenegraph.SimpleAppearance> extends GlrAppearance<E> {
-	private boolean m_isShaded;
-	private boolean m_isAmbientLinkedToDiffuse;
-	private boolean m_isMaterialActuallyShowing;
-	private boolean m_isMaterialAlphaBlended;
-	private float[] m_ambient = { Float.NaN, Float.NaN, Float.NaN, Float.NaN };
-	private float[] m_diffuse = { Float.NaN, Float.NaN, Float.NaN, Float.NaN };
-	private float[] m_specular = { Float.NaN, Float.NaN, Float.NaN, Float.NaN };
-	private float[] m_emissive = { Float.NaN, Float.NaN, Float.NaN, Float.NaN };
-	private float m_opacity = Float.NaN;
-	private float m_shininess = Float.NaN;
-	private int m_polygonMode;
-	private boolean m_isEthereal;
-
 	@Override
 	public boolean isActuallyShowing() {
-		return m_isMaterialActuallyShowing;
+		return this.isMaterialActuallyShowing;
 	}
 
 	@Override
 	public boolean isAlphaBlended() {
-		return m_isMaterialAlphaBlended;
+		return this.isMaterialAlphaBlended;
 	}
 
 	@Override
 	public boolean isAllAlphaBlended() {
-		return m_isMaterialAlphaBlended;
+		return this.isMaterialAlphaBlended;
 	}
 
 	@Override
 	public boolean isEthereal() {
-		return m_isEthereal;
+		return this.isEthereal;
 	}
 
 	@Override
 	public void setPipelineState( RenderContext rc, int face ) {
-		rc.setIsShadingEnabled( m_isShaded );
+		rc.setIsShadingEnabled( this.isShaded );
 		rc.gl.glDisable( GL_TEXTURE_2D );
 		//todo: investigate if specular, emissive, ambient should use an opacity of 1.0f
-		if( m_isShaded ) {
-			rc.gl.glMaterialf( face, GL_SHININESS, m_shininess );
-			rc.setMaterial( face, GL_SPECULAR, m_specular, m_opacity );
-			rc.setMaterial( face, GL_EMISSION, m_emissive, m_opacity );
-			if( m_isAmbientLinkedToDiffuse ) {
+		if( this.isShaded ) {
+			rc.gl.glMaterialf( face, GL_SHININESS, this.shininess );
+			rc.setMaterial( face, GL_SPECULAR, this.specular, this.opacity );
+			rc.setMaterial( face, GL_EMISSION, this.emissive, this.opacity );
+			if( this.isAmbientLinkedToDiffuse ) {
 				rc.gl.glColorMaterial( face, GL_AMBIENT_AND_DIFFUSE );
 			} else {
-				rc.setMaterial( face, GL_AMBIENT, m_ambient, m_opacity );
+				rc.setMaterial( face, GL_AMBIENT, this.ambient, this.opacity );
 				rc.gl.glColorMaterial( face, GL_DIFFUSE );
 			}
 		}
-		rc.setColor( m_diffuse, m_opacity );
-		rc.gl.glPolygonMode( face, m_polygonMode );
+		rc.setColor( this.diffuse, this.opacity );
+		rc.gl.glPolygonMode( face, this.polygonMode );
 		//todo
-		if( m_isEthereal ) {
+		if( this.isEthereal ) {
 			rc.gl.glDepthFunc( GL_LEQUAL );
 		} else {
 			rc.gl.glDepthFunc( GL_LESS );
@@ -75,56 +62,69 @@ public class GlrSimpleAppearance<E extends edu.cmu.cs.dennisc.scenegraph.SimpleA
 	}
 
 	private void updateOpacityRelatedBooleans() {
-		float opacity = m_opacity * m_diffuse[ 3 ];
-		m_isMaterialActuallyShowing = opacity > 0.0f;
-		m_isMaterialAlphaBlended = opacity < 1.0f;
+		float opacity = this.opacity * this.diffuse[ 3 ];
+		this.isMaterialActuallyShowing = opacity > 0.0f;
+		this.isMaterialAlphaBlended = opacity < 1.0f;
 	}
 
 	@Override
 	protected void propertyChanged( edu.cmu.cs.dennisc.property.InstanceProperty<?> property ) {
 		if( property == owner.ambientColor ) {
-			owner.ambientColor.getValue().getAsArray( m_ambient );
-			m_isAmbientLinkedToDiffuse = Float.isNaN( m_ambient[ 0 ] );
+			owner.ambientColor.getValue().getAsArray( this.ambient );
+			this.isAmbientLinkedToDiffuse = Float.isNaN( this.ambient[ 0 ] );
 		} else if( property == owner.diffuseColor ) {
-			owner.diffuseColor.getValue().getAsArray( m_diffuse );
-			updateOpacityRelatedBooleans();
+			owner.diffuseColor.getValue().getAsArray( this.diffuse );
+			this.updateOpacityRelatedBooleans();
+		} else if( property == owner.opacity ) {
+			this.opacity = owner.opacity.getValue();
+			this.updateOpacityRelatedBooleans();
+		} else if( property == owner.specularHighlightColor ) {
+			owner.specularHighlightColor.getValue().getAsArray( this.specular );
+		} else if( property == owner.specularHighlightExponent ) {
+			this.shininess = owner.specularHighlightExponent.getValue();
+		} else if( property == owner.emissiveColor ) {
+			owner.emissiveColor.getValue().getAsArray( this.emissive );
 		} else if( property == owner.fillingStyle ) {
 			edu.cmu.cs.dennisc.scenegraph.FillingStyle fillingStyle = owner.fillingStyle.getValue();
 			if( fillingStyle.equals( edu.cmu.cs.dennisc.scenegraph.FillingStyle.SOLID ) ) {
-				m_polygonMode = GL_FILL;
+				this.polygonMode = GL_FILL;
 			} else if( fillingStyle.equals( edu.cmu.cs.dennisc.scenegraph.FillingStyle.WIREFRAME ) ) {
-				m_polygonMode = GL_LINE;
+				this.polygonMode = GL_LINE;
 			} else if( fillingStyle.equals( edu.cmu.cs.dennisc.scenegraph.FillingStyle.POINTS ) ) {
-				m_polygonMode = GL_POINT;
+				this.polygonMode = GL_POINT;
 			} else {
 				throw new RuntimeException();
 			}
 		} else if( property == owner.shadingStyle ) {
 			edu.cmu.cs.dennisc.scenegraph.ShadingStyle shadingStyle = owner.shadingStyle.getValue();
 			if( ( shadingStyle == null ) || shadingStyle.equals( edu.cmu.cs.dennisc.scenegraph.ShadingStyle.NONE ) ) {
-				m_isShaded = false;
+				this.isShaded = false;
 			} else if( shadingStyle.equals( edu.cmu.cs.dennisc.scenegraph.ShadingStyle.FLAT ) ) {
-				m_isShaded = true;
+				this.isShaded = true;
 				//todo
 			} else if( shadingStyle.equals( edu.cmu.cs.dennisc.scenegraph.ShadingStyle.SMOOTH ) ) {
-				m_isShaded = true;
+				this.isShaded = true;
 				//todo
 			} else {
 				throw new RuntimeException();
 			}
-		} else if( property == owner.opacity ) {
-			m_opacity = owner.opacity.getValue();
-			updateOpacityRelatedBooleans();
-		} else if( property == owner.specularHighlightColor ) {
-			owner.specularHighlightColor.getValue().getAsArray( m_specular );
-		} else if( property == owner.specularHighlightExponent ) {
-			m_shininess = owner.specularHighlightExponent.getValue();
-		} else if( property == owner.emissiveColor ) {
-			owner.emissiveColor.getValue().getAsArray( m_emissive );
 		} else if( property == owner.isEthereal ) {
-			m_isEthereal = owner.isEthereal.getValue();
+			this.isEthereal = owner.isEthereal.getValue();
 		} else {
 			super.propertyChanged( property );
 		}
 	}
+
+	private boolean isShaded;
+	private boolean isAmbientLinkedToDiffuse;
+	private boolean isMaterialActuallyShowing;
+	private boolean isMaterialAlphaBlended;
+	private final float[] ambient = { Float.NaN, Float.NaN, Float.NaN, Float.NaN };
+	private final float[] diffuse = { Float.NaN, Float.NaN, Float.NaN, Float.NaN };
+	private final float[] specular = { Float.NaN, Float.NaN, Float.NaN, Float.NaN };
+	private final float[] emissive = { Float.NaN, Float.NaN, Float.NaN, Float.NaN };
+	private float opacity = Float.NaN;
+	private float shininess = Float.NaN;
+	private int polygonMode;
+	private boolean isEthereal;
 }
