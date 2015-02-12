b459783c62ddfad6a40964669a910480b48e1ed9
Dennis Cosgrove <dennisc@cs.cmu.edu>
2015-02-12 14:49:52 -0600

    use new field icon generation
    Former-commit-id: 887fc0e44e4ff19a3f556434f98e1ee9e701030b

--- core/ide/src/main/java/org/alice/ide/instancefactory/croquet/views/icons/IndirectCurrentAccessibleTypeIcon.java
+++ core/ide/src/main/java/org/alice/ide/instancefactory/croquet/views/icons/IndirectCurrentAccessibleTypeIcon.java
@@ -56,7 +56,9 @@ public enum IndirectCurrentAccessibleTypeIcon implements javax.swing.Icon {
 			org.lgna.croquet.icon.IconFactory rv = null;
 			if( instanceFactory instanceof org.alice.ide.instancefactory.ThisFieldAccessFactory ) {
 				org.alice.ide.instancefactory.ThisFieldAccessFactory thisFieldAccessFactory = (org.alice.ide.instancefactory.ThisFieldAccessFactory)instanceFactory;
-				rv = org.alice.stageide.icons.IconFactoryManager.getIconFactoryForField( thisFieldAccessFactory.getField() );
+				org.lgna.project.ast.UserField field = thisFieldAccessFactory.getField();
+				org.alice.ide.iconfactory.IconFactoryManager iconFactoryManager = org.alice.ide.IDE.getActiveInstance().getDocumentFrame().getIconFactoryManager();
+				rv = iconFactoryManager.getIconFactory( field, org.alice.stageide.icons.IconFactoryManager.getIconFactoryForField( field ) );
 			}
 			if( rv != null ) {
 				//pass
