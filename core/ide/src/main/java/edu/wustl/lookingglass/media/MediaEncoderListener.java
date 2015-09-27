964b2f91d62c0b2ee06a598b1bb64f121be46bb0
Kyle J. Harms <kyle.harms@wustl.edu>
2015-09-27 17:25:38 -0500

    updated copyright for ide
    Former-commit-id: b0358c4740b108d5c6b26ab0b315c17167ad5f40

--- core/ide/src/main/java/edu/wustl/lookingglass/media/MediaEncoderListener.java
+++ core/ide/src/main/java/edu/wustl/lookingglass/media/MediaEncoderListener.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2008-2013, Washington University in St. Louis. All rights reserved.
+ * Copyright (c) 2008-2015, Washington University in St. Louis. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met:
--- core/ide/src/main/java/org/alice/media/audio/MixingFloatAudioInputStream.java
+++ core/ide/src/main/java/org/alice/media/audio/MixingFloatAudioInputStream.java
@@ -1,36 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2015, Carnegie Mellon University. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Products derived from the software may not be called "Alice", nor may
+ *    "Alice" appear in their name, without prior written permission of
+ *    Carnegie Mellon University.
+ *
+ * 4. All advertising materials mentioning features or use of this software must
+ *    display the following acknowledgement: "This product includes software
+ *    developed by Carnegie Mellon University"
+ *
+ * 5. The gallery of art assets and animations provided with this software is
+ *    contributed by Electronic Arts Inc. and may be used for personal,
+ *    non-commercial, and academic use only. Redistributions of any program
+ *    source code that utilizes The Sims 2 Assets must also retain the copyright
+ *    notice, list of conditions and the disclaimer contained in
+ *    The Alice 3.0 Art Gallery License.
+ *
+ * DISCLAIMER:
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+ * ANY AND ALL EXPRESS, STATUTORY OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,  FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, AND NON-INFRINGEMENT ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE AUTHORS, COPYRIGHT OWNERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING FROM OR OTHERWISE RELATING TO
+ * THE USE OF OR OTHER DEALINGS WITH THE SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
 /*
  *	MixingFloatAudioInputStream
  *
  *	This file is part of jsresources.org
- */
-
-/*
- * Copyright (c) 2006 by Florian Bomers
- * Copyright (c) 1999 - 2001 by Matthias Pfisterer
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * - Redistributions of source code must retain the above copyright notice,
- *   this list of conditions and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright
- *   notice, this list of conditions and the following disclaimer in the
- *   documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package org.alice.media.audio;
@@ -58,7 +71,7 @@ import javax.sound.sampled.AudioSystem;
  * <p>
  * This class uses the FloatSampleBuffer for easy conversion using normalized
  * samples in the buffers.
- * 
+ *
  * @author Florian Bomers
  * @author Matthias Pfisterer
  */
@@ -125,8 +138,7 @@ public class MixingFloatAudioInputStream extends AudioInputStream {
 		this.totalBytes = (long)( this.bytesPerSecond * this.length );
 	}
 
-	public float secondsProcessed()
-	{
+	public float secondsProcessed() {
 		return this.bytesRead / this.bytesPerSecond;
 	}
 
@@ -151,12 +163,10 @@ public class MixingFloatAudioInputStream extends AudioInputStream {
 	}
 
 	@Override
-	public int read( byte[] abData, int nOffset, int nLength ) throws IOException
-	{
+	public int read( byte[] abData, int nOffset, int nLength ) throws IOException {
 		float secondsElapsed = this.secondsProcessed();
 
-		if( this.bytesRead >= this.totalBytes )
-		{
+		if( this.bytesRead >= this.totalBytes ) {
 			return -1;
 		}
 
@@ -173,8 +183,7 @@ public class MixingFloatAudioInputStream extends AudioInputStream {
 		while( streamIterator.hasNext() ) {
 			ScheduledAudioStream scheduledStream = (ScheduledAudioStream)streamIterator.next();
 			//skip streams that haven't started yet
-			if( scheduledStream.getStartTime() > secondsElapsed )
-			{
+			if( scheduledStream.getStartTime() > secondsElapsed ) {
 				continue;
 			}
 			AudioInputStream stream = scheduledStream.getAudioStream();
@@ -217,9 +226,9 @@ public class MixingFloatAudioInputStream extends AudioInputStream {
 				// deletion).
 				int maxSamples = Math.min( mixBuffer.getSampleCount(),
 						readBuffer.getSampleCount() );
-				// in a loop, add each "read" sample to the mix buffer
-				// can only mix as many samples as available. Also apply the
-				// attenuation factor.
+						// in a loop, add each "read" sample to the mix buffer
+						// can only mix as many samples as available. Also apply the
+						// attenuation factor.
 
 				// Note1: the attenuation factor could also be applied only once
 				// in a separate loop after mixing all the streams together,
@@ -246,8 +255,7 @@ public class MixingFloatAudioInputStream extends AudioInputStream {
 
 		long bytesProcessed = maxMixed * getFormat().getFrameSize();
 
-		if( ( bytesProcessed + this.bytesRead ) > this.totalBytes )
-		{
+		if( ( bytesProcessed + this.bytesRead ) > this.totalBytes ) {
 			bytesProcessed = this.totalBytes - this.bytesRead;
 		}
 
