9c50fa47170f4481a22d9e248cac06fae576107e
dculyba <dculyba@andrew.cmu.edu>
2016-08-09 13:47:46 -0400

    added missing java file
    Former-commit-id: cab448e5a3be5116c054242ea55df12a03a2adf9

---
+++ core/story-api-migration/src/main/java/org/lgna/project/migration/ast/ChangeDeclaringClassForAxesSetVehicle.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2006-2012, Carnegie Mellon University. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Products derived from the software may not be called "Alice", nor may
+ *    "Alice" appear in their name, without prior written permission of
+ *    Carnegie Mellon University.
+ *
+ * 4. All advertising materials mentioning features or use of this software must
+ *    display the following acknowledgement: "This product includes software
+ *    developed by Carnegie Mellon University"
+ *
+ * 5. The gallery of art assets and animations provided with this software is
+ *    contributed by Electronic Arts Inc. and may be used for personal,
+ *    non-commercial, and academic use only. Redistributions of any program
+ *    source code that utilizes The Sims 2 Assets must also retain the copyright
+ *    notice, list of conditions and the disclaimer contained in
+ *    The Alice 3.0 Art Gallery License.
+ *
+ * DISCLAIMER:
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+ * ANY AND ALL EXPRESS, STATUTORY OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,  FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, AND NON-INFRINGEMENT ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE AUTHORS, COPYRIGHT OWNERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING FROM OR OTHERWISE RELATING TO
+ * THE USE OF OR OTHER DEALINGS WITH THE SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.lgna.project.migration.ast;
+
+import org.lgna.project.Project;
+import org.lgna.project.ast.MethodInvocation;
+
+/**
+ * @author dculyba
+ */
+public class ChangeDeclaringClassForAxesSetVehicle extends MethodInvocationAstMigration {
+
+	public ChangeDeclaringClassForAxesSetVehicle( org.lgna.project.Version minimumVersion, org.lgna.project.Version maximumVersion ) {
+		super( minimumVersion, maximumVersion );
+	}
+
+	@Override
+	protected void migrate( MethodInvocation methodInvocation, Project projectIfApplicable ) {
+		org.lgna.project.ast.AbstractMethod method = methodInvocation.method.getValue();
+		if( method instanceof org.lgna.project.ast.JavaMethod ) {
+			org.lgna.project.ast.JavaMethod javaMethod = (org.lgna.project.ast.JavaMethod)method;
+			if( ( javaMethod.getDeclaringType() == org.lgna.project.ast.JavaType.getInstance( org.lgna.story.SAxes.class ) ) && javaMethod.getName().equals( "setVehicle" ) ) {
+				org.lgna.project.ast.AbstractMethod replacementMethod = org.lgna.project.ast.AstUtilities.lookupMethod( org.lgna.story.MutableRider.class, "setVehicle", org.lgna.story.SThing.class );
+				methodInvocation.method.setValue( replacementMethod );
+				edu.cmu.cs.dennisc.java.util.logging.Logger.outln( "updating setVehicle method on Axes object" );
+			}
+		}
+	}
+
+}
