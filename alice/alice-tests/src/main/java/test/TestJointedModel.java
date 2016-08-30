7e85851d706fe36f30f4e1fa41853291a2817161
dculyba <dculyba@andrew.cmu.edu>
2016-08-30 16:12:29 -0400

    minor fixes for test programs to deal with code changes
    Former-commit-id: 8b9727cf7d9e8bacff020b9af2976633a42a1a78

--- alice/alice-tests/src/main/java/test/TestJointedModel.java
+++ alice/alice-tests/src/main/java/test/TestJointedModel.java
@@ -1,43 +1,43 @@
 /*
  * Copyright (c) 2006-2010, Carnegie Mellon University. All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without 
+ *
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
- * 1. Redistributions of source code must retain the above copyright notice, 
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  *
- * 2. Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
  *
- * 3. Products derived from the software may not be called "Alice", nor may 
- *    "Alice" appear in their name, without prior written permission of 
+ * 3. Products derived from the software may not be called "Alice", nor may
+ *    "Alice" appear in their name, without prior written permission of
  *    Carnegie Mellon University.
  *
  * 4. All advertising materials mentioning features or use of this software must
- *    display the following acknowledgement: "This product includes software 
+ *    display the following acknowledgement: "This product includes software
  *    developed by Carnegie Mellon University"
  *
- * 5. The gallery of art assets and animations provided with this software is 
- *    contributed by Electronic Arts Inc. and may be used for personal, 
- *    non-commercial, and academic use only. Redistributions of any program 
+ * 5. The gallery of art assets and animations provided with this software is
+ *    contributed by Electronic Arts Inc. and may be used for personal,
+ *    non-commercial, and academic use only. Redistributions of any program
  *    source code that utilizes The Sims 2 Assets must also retain the copyright
- *    notice, list of conditions and the disclaimer contained in 
+ *    notice, list of conditions and the disclaimer contained in
  *    The Alice 3.0 Art Gallery License.
- * 
+ *
  * DISCLAIMER:
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.  
- * ANY AND ALL EXPRESS, STATUTORY OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,  FITNESS FOR A 
- * PARTICULAR PURPOSE, TITLE, AND NON-INFRINGEMENT ARE DISCLAIMED. IN NO EVENT 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+ * ANY AND ALL EXPRESS, STATUTORY OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,  FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, AND NON-INFRINGEMENT ARE DISCLAIMED. IN NO EVENT
  * SHALL THE AUTHORS, COPYRIGHT OWNERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR CONSEQUENTIAL DAMAGES 
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING FROM OR OTHERWISE RELATING TO 
- * THE USE OF OR OTHER DEALINGS WITH THE SOFTWARE, EVEN IF ADVISED OF THE 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING FROM OR OTHERWISE RELATING TO
+ * THE USE OF OR OTHER DEALINGS WITH THE SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
@@ -138,27 +138,19 @@ class TestJointedModel extends SProgram {
 	private static java.util.Map<Integer, org.lgna.story.resources.JointId> mapHashCodeToJointId = edu.cmu.cs.dennisc.java.util.Maps.newHashMap();
 	static {
 		for( org.lgna.story.resources.JointId rootJointId : org.lgna.story.resources.BipedResource.JOINT_ID_ROOTS ) {
-			fillInMap( rootJointId );
+			fillInMap( rootJointId, org.lgna.story.resources.biped.OgreResource.GREEN );
 		}
 	}
 
-	private static void fillInMap( org.lgna.story.resources.JointId jointId ) {
+	private static void fillInMap( org.lgna.story.resources.JointId jointId, org.lgna.story.resources.JointedModelResource forResource ) {
 		mapHashCodeToJointId.put( jointId.hashCode(), jointId );
-		for( org.lgna.story.resources.JointId childId : jointId.getDeclaredChildren() ) {
-			fillInMap( childId );
+		for( org.lgna.story.resources.JointId childId : jointId.getDeclaredChildren( forResource ) ) {
+			fillInMap( childId, forResource );
 		}
 	}
 
 	private final SCamera camera = new SCamera();
-	private final MyBiped susan = new MyBiped(
-			new AdultPersonResource(
-					Gender.FEMALE,
-					BaseSkinTone.getRandom(),
-					BaseEyeColor.getRandom(),
-					FemaleAdultHairBraids.BLACK,
-					0.5,
-					FemaleAdultFullBodyOutfitAmbulanceDriver.BLUE
-			) );
+	private final MyBiped susan = new MyBiped( new AdultPersonResource( Gender.FEMALE, BaseSkinTone.getRandom(), BaseEyeColor.getRandom(), FemaleAdultHairBraids.BLACK, 0.5, FemaleAdultFullBodyOutfitAmbulanceDriver.BLUE ) );
 	private final MyBiped ogre = new MyBiped( org.lgna.story.resources.biped.OgreResource.BROWN );
 	private final SThingMarker marker = new SThingMarker();
 	private final TestScene testScene = new TestScene( camera, susan, marker );
--- alice/alice-tests/src/main/java/test/iconrender/IconRenderTest.java
+++ alice/alice-tests/src/main/java/test/iconrender/IconRenderTest.java
@@ -110,26 +110,26 @@ public class IconRenderTest {
 						@Override
 						public void render( Object context ) {
 							edu.cmu.cs.dennisc.render.gl.imp.GlrRenderContext glrRenderContext = (edu.cmu.cs.dennisc.render.gl.imp.GlrRenderContext)context;
-							javax.media.opengl.GL2 gl = glrRenderContext.getDrawable().getGL().getGL2();
+							com.jogamp.opengl.GL2 gl = glrRenderContext.getDrawable().getGL().getGL2();
 							java.awt.Rectangle viewport = glrRenderContext.getViewport();
 							edu.cmu.cs.dennisc.color.Color4f backgroundColor = rImageBuffer.getBackgroundColor();
 							final boolean IS_ALPHA = backgroundColor == null;
 							int clearMask;
 							if( IS_ALPHA ) {
-								clearMask = javax.media.opengl.GL.GL_DEPTH_BUFFER_BIT;
+								clearMask = com.jogamp.opengl.GL.GL_DEPTH_BUFFER_BIT;
 							} else {
 								gl.glClearColor( backgroundColor.red, backgroundColor.green, backgroundColor.blue, backgroundColor.alpha );
-								clearMask = javax.media.opengl.GL.GL_COLOR_BUFFER_BIT | javax.media.opengl.GL.GL_DEPTH_BUFFER_BIT;
+								clearMask = com.jogamp.opengl.GL.GL_COLOR_BUFFER_BIT | com.jogamp.opengl.GL.GL_DEPTH_BUFFER_BIT;
 							}
-							gl.glEnable( javax.media.opengl.GL.GL_SCISSOR_TEST );
+							gl.glEnable( com.jogamp.opengl.GL.GL_SCISSOR_TEST );
 							gl.glScissor( viewport.x, viewport.y, viewport.width, viewport.height );
 							gl.glClear( clearMask );
-							gl.glDisable( javax.media.opengl.GL.GL_SCISSOR_TEST );
+							gl.glDisable( com.jogamp.opengl.GL.GL_SCISSOR_TEST );
 
 							gl.glViewport( viewport.x, viewport.y, viewport.width, viewport.height );
 
-							javax.media.opengl.glu.GLU glu = new javax.media.opengl.glu.GLU();
-							gl.glMatrixMode( javax.media.opengl.fixedfunc.GLMatrixFunc.GL_PROJECTION );
+							com.jogamp.opengl.glu.GLU glu = new com.jogamp.opengl.glu.GLU();
+							gl.glMatrixMode( com.jogamp.opengl.fixedfunc.GLMatrixFunc.GL_PROJECTION );
 							gl.glLoadIdentity();
 
 							glu.gluPerspective( 45.0, viewport.width / (double)viewport.height, 0.1, 100.0 );
@@ -141,20 +141,20 @@ public class IconRenderTest {
 							rc.setGL( gl );
 							rc.initialize();
 
-							gl.glMatrixMode( javax.media.opengl.fixedfunc.GLMatrixFunc.GL_MODELVIEW );
+							gl.glMatrixMode( com.jogamp.opengl.fixedfunc.GLMatrixFunc.GL_MODELVIEW );
 							gl.glLoadIdentity();
 							glu.gluLookAt( p.x + 2, p.y + 2, p.z - 2, p.x, p.y, p.z, 0, 1, 0 );
 
 							edu.cmu.cs.dennisc.render.gl.imp.adapters.GlrScene sceneAdapter = edu.cmu.cs.dennisc.render.gl.imp.adapters.AdapterFactory.getAdapterFor( sgScene );
 							sceneAdapter.setupAffectors( rc );
-							gl.glEnable( javax.media.opengl.GL.GL_DEPTH_TEST );
-							//							gl.glDisable( javax.media.opengl.GL.GL_BLEND );
+							gl.glEnable( com.jogamp.opengl.GL.GL_DEPTH_TEST );
+							//							gl.glDisable( com.jogamp.opengl.GL.GL_BLEND );
 							transformableAdapter.renderOpaque( rc );
-							//							gl.glEnable( javax.media.opengl.GL.GL_BLEND );
-							//							gl.glBlendFunc( javax.media.opengl.GL.GL_SRC_ALPHA, javax.media.opengl.GL.GL_ONE_MINUS_SRC_ALPHA );
+							//							gl.glEnable( com.jogamp.opengl.GL.GL_BLEND );
+							//							gl.glBlendFunc( com.jogamp.opengl.GL.GL_SRC_ALPHA, com.jogamp.opengl.GL.GL_ONE_MINUS_SRC_ALPHA );
 							//							transformableAdapter.renderAlphaBlended( rc );
-							//							gl.glDisable( javax.media.opengl.GL.GL_BLEND );
-							gl.glDisable( javax.media.opengl.GL.GL_DEPTH_TEST );
+							//							gl.glDisable( com.jogamp.opengl.GL.GL_BLEND );
+							gl.glDisable( com.jogamp.opengl.GL.GL_DEPTH_TEST );
 						}
 					}, new java.awt.Rectangle( surfaceSize.width, surfaceSize.height, surfaceSize.width, surfaceSize.height ), rImageBuffer, edu.cmu.cs.dennisc.render.ImageOrientationRequirement.RIGHT_SIDE_UP_REQUIRED, new edu.cmu.cs.dennisc.render.ImageCaptureObserver() {
 						@Override
