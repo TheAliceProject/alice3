2c12f68bf7a57d3774b34687d388e9f237810a80
Daniel Abeshouse <dabeshou@andrew.cmu.edu>
2017-09-08 12:28:39 -0400

    Add override to make TC compiler happy
    Former-commit-id: 099d15dc33f09eebb7340a2ed072413c46a07b87

--- core/util/src/main/java/org/lgna/common/ComponentExecutor.java
+++ core/util/src/main/java/org/lgna/common/ComponentExecutor.java
@@ -1,7 +1,5 @@
 package org.lgna.common;
 
-import java.util.Collection;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -14,6 +12,7 @@ public class ComponentExecutor implements Runnable{
 		this.target = target;
 	}
 
+	@Override
 	public void run() {
 		org.lgna.common.ProgramClosedException.invokeAndCatchProgramClosedException( this.target );
 	}
