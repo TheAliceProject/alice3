ec30f2ea0429ccbbe105c1d715e626b6feccf019
Daniel Abeshouse <dabeshou@andrew.cmu.edu>
2018-03-09 13:55:23 -0500

    WIP Replace ModelResource with ModelStructure
    Former-commit-id: 4a8b2527faa5f0adc375047054c401f4bc934da1

---
+++ core/story-api/src/main/java/org/lgna/story/resources/ReflectionBasedResource.java
@@ -0,0 +1,57 @@
+package org.lgna.story.resources;
+
+import edu.cmu.cs.dennisc.math.AxisAlignedBox;
+import org.lgna.story.SThing;
+import org.lgna.story.implementation.JointedModelImp;
+
+public class ReflectionBasedResource<I extends JointedModelImp,T extends SThing> implements ModelStructure<I,T>{
+
+	Class<? extends org.lgna.story.resources.ModelResource> resourceCls;
+
+	public ReflectionBasedResource() {
+
+	}
+
+	@Override
+	public JointedModelImp.JointImplementationAndVisualDataFactory<JointedModelResource> getImplementationAndVisualFactory() {
+		//	return this.resourceType.getFactory( this );
+		return null;
+	}
+
+	@Override public JointId[] getRootJointIds() {
+		return new JointId[0];
+	}
+
+	@Override
+	public I createImplementation( T abstraction ) {
+		return null;//resourceCls.createImplementation(abstraction);
+	}
+
+	@Override public String[] getTags() {
+		return new String[0];
+	}
+
+	@Override public String[] getGroupTags() {
+		return new String[0];
+	}
+
+	@Override public String getModelClassName() {
+		return "";
+	}
+
+	@Override public String getInternalModelClassName() {
+		return "";
+	}
+
+	@Override public String[] getThemeTags() {
+		return new String[0];
+	}
+
+	@Override public AxisAlignedBox getBoundingBox() {
+		return null;
+	}
+
+	@Override public boolean getPlaceOnGround() {
+		return false;
+	}
+}
