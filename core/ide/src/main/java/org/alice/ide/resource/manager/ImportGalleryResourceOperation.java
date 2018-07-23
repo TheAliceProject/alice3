e5cb98232383b05d5d6d60ae20edaa2771290150
Daniel Abeshouse <dabeshou@andrew.cmu.edu>
2018-07-23 10:09:07 -0400

    Add import model to file menu. Connect file open and import to it

--- core/ide/src/main/java/org/alice/ide/resource/manager/ImportGalleryResourceOperation.java
+++ core/ide/src/main/java/org/alice/ide/resource/manager/ImportGalleryResourceOperation.java
@@ -1,26 +1,48 @@
 package org.alice.ide.resource.manager;
 
+import edu.cmu.cs.dennisc.javax.swing.icons.LineAxisIcon;
+import org.alice.ide.icons.Icons;
 import org.alice.stageide.gallerybrowser.ImportGalleryResourceComposite;
-import org.lgna.croquet.ActionOperation;
+import org.alice.stageide.icons.PlusIconFactory;
 import org.lgna.croquet.Application;
-import org.lgna.croquet.Element;
+import org.lgna.croquet.Model;
+import org.lgna.croquet.SingleThreadIteratingOperation;
 import org.lgna.croquet.history.CompletionStep;
+import org.lgna.croquet.history.Step;
 
+import java.awt.*;
+import java.util.Iterator;
+import java.util.List;
 import java.util.UUID;
 
-public class ImportGalleryResourceOperation  extends ActionOperation {
-    public ImportGalleryResourceOperation() {
-        super( Application.DOCUMENT_UI_GROUP, UUID.fromString( "178d0562-a78b-40a2-907e-3cef46a4811d" ) );
-    }
+public class ImportGalleryResourceOperation extends SingleThreadIteratingOperation {
+	private final ImportGalleryResourceComposite importGalleryResourceComposite = new ImportGalleryResourceComposite();
 
-    @Override
-    protected Class<? extends Element> getClassUsedForLocalization() {
-        return ImportGalleryResourceComposite.class;
-    }
+	public ImportGalleryResourceOperation() {
+		super( Application.DOCUMENT_UI_GROUP, UUID.fromString( "178d0562-a78b-40a2-907e-3cef46a4811d" ) );
+		this.setButtonIcon( new LineAxisIcon( Icons.FOLDER_ICON_SMALL, PlusIconFactory.getInstance().getIcon(
+				new Dimension( Icons.FOLDER_ICON_SMALL.getIconWidth(), Icons.FOLDER_ICON_SMALL.getIconHeight() ) ) ) );
+	}
 
+	@Override
+	protected boolean hasNext( CompletionStep<?> step, java.util.List<Step<?>> subSteps, Iterator<Model> iteratingData ) {
+		return subSteps.size() < 2;
+	}
 
-    @Override
-    protected void perform( CompletionStep<?> step ) {
-        //TODO: What should we do here? Should we access the ImportGalleryResourceComposite somehow and display it?
-    }
+	@Override
+	protected Model getNext( CompletionStep<?> step, java.util.List<Step<?>> subSteps, Iterator<Model> iteratingData ) {
+		switch (subSteps.size()) {
+		case 0:
+			return importGalleryResourceComposite.getBrowseOperation();
+		case 1:
+			return importGalleryResourceComposite.getLoadOperation();
+		default:
+			return null;
+		}
+	}
+
+	@Override
+	protected void handleSuccessfulCompletionOfSubModels( CompletionStep<?> step, List<Step<?>> subSteps ) {
+		step.finish();
+	}
 }
