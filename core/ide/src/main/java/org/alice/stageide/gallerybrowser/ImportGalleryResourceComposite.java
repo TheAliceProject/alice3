d3a6fe647233f995bf3634a24bb3adb76aaae65d
Dave Culyba <dculyba@andrew.cmu.edu>
2018-08-03 15:46:16 -0400

    Removed "getChildren" from JointId
    Big overhaul on joints:
    Removed "getChildren" from JointId to eliminate dependency on walking the tree from root joints. The new algorithm uses reflection to get all the fields and then creates a JointImp tree by using the "getParent" field. This means a given joint resource can know all it joints without the weird scenario of the root joint of the BipedResource needing a way to walk down to all the different biped resource varirations.
    
    Got rid of most of the getRootJointIds() calls
    Shifted some joint calls to get jointArrayIds instead of full implementations
    Reworked JointedModelImp to initialize itself using "getParent" instead of "getChildren" on the joints
      -added the function "getAllJoints" which gets all the joint ids for the given resource
      -this includes getting all the implied ids from joint arrays
    Added idea of "joint parent" and "joint children" to JointImp
      -this is for traversing the joint tree
    Commented out some Pose oriented code that is still using "getChildren"
    Dynamic resources have "getRootJointIds" added to them all (although this is now becoming deprecated)

--- core/ide/src/main/java/org/alice/stageide/gallerybrowser/ImportGalleryResourceComposite.java
+++ core/ide/src/main/java/org/alice/stageide/gallerybrowser/ImportGalleryResourceComposite.java
@@ -4,6 +4,9 @@ import edu.cmu.cs.dennisc.java.io.FileUtilities;
 import edu.cmu.cs.dennisc.java.lang.SystemUtilities;
 import edu.cmu.cs.dennisc.javax.swing.option.OkDialog;
 import edu.cmu.cs.dennisc.math.AxisAlignedBox;
+import edu.cmu.cs.dennisc.scenegraph.Component;
+import edu.cmu.cs.dennisc.scenegraph.Composite;
+import edu.cmu.cs.dennisc.scenegraph.Joint;
 import edu.cmu.cs.dennisc.scenegraph.SkeletonVisual;
 import org.alice.ide.icons.Icons;
 import org.alice.stageide.StageIDE;
@@ -15,17 +18,27 @@ import org.lgna.croquet.edits.Edit;
 import org.lgna.croquet.history.CompletionStep;
 import org.lgna.croquet.views.Panel;
 import org.lgna.project.ProjectVersion;
+import org.lgna.project.annotations.FieldTemplate;
+import org.lgna.project.annotations.Visibility;
 import org.lgna.project.io.GalleryModelIo;
+import org.lgna.story.implementation.alice.AliceResourceClassUtilities;
 import org.lgna.story.resources.DynamicResource;
+import org.lgna.story.resources.JointId;
+import org.lgna.story.resources.JointedModelResource;
+import org.lgna.story.resources.ModelResource;
 import org.lgna.story.resourceutilities.AdaptiveRecenteringThumbnailMaker;
 import org.lgna.story.resourceutilities.JointedModelColladaImporter;
 import org.lgna.story.resourceutilities.ModelLoadingException;
+import org.lgna.story.resourceutilities.PipelineException;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
 import java.util.logging.Logger;
 
@@ -123,9 +136,53 @@ public class ImportGalleryResourceComposite extends ValueCreatorInputDialogCoreC
 			return;
 		}
 
-		BufferedImage thumbnail = AdaptiveRecenteringThumbnailMaker.getInstance( 160, 120 ).createThumbnail( sv );
 		// TODO get a user name
-		ModelManifest modelManifest = createSimpleManifest( sv, "Dave", "Biped" );
+		String creatorName = "Dave";
+		// TODO get a parent class
+		String parentClassName = "Prop";
+		Class<? extends ModelResource> parentClass = AliceResourceClassUtilities.getResourceClassForAliceName(parentClassName);
+
+		List<ModelManifest.Joint> baseJoints = getBaseJoints(parentClass);
+
+		if (sv.skeleton.getValue() == null && !baseJoints.isEmpty()) {
+			PipelineException p = new PipelineException("Required skeleton missing on " + sv.getName() + ". Base class is " + parentClass);
+			final OkDialog.Builder builder = new OkDialog.Builder( p.getLocalizedMessage() + "\nTry to correct it and reimport." );
+			builder.title( "Problem during import" );
+			builder.buildAndShow();
+			return;
+		}
+
+		List<ModelManifest.Joint> modelJoints = getModelJoints(sv);
+		List<ModelManifest.Joint> missingJoints = getMissingJoints(modelJoints, baseJoints);
+		if (!missingJoints.isEmpty()) {
+			StringBuilder missingJointsMessage = new StringBuilder("Base class "+parentClass+"requires the following joints:");
+			for (ModelManifest.Joint joint : baseJoints) {
+				missingJointsMessage.append('\t');
+				missingJointsMessage.append(joint.name+":");
+				if (missingJoints.contains(joint)) {
+					missingJointsMessage.append("\tMISSING");
+				}
+				else {
+					missingJointsMessage.append("\tFOUND");
+				}
+				missingJointsMessage.append('\n');
+			}
+			missingJointsMessage.append("Correct for these missing joints and reimport.");
+			final OkDialog.Builder builder = new OkDialog.Builder( missingJointsMessage.toString() );
+			builder.title( "Problem during import" );
+			builder.buildAndShow();
+			return;
+		}
+		List<ModelManifest.Joint> extraJoints = getExtraJoints(modelJoints, baseJoints, true);
+
+		ModelManifest modelManifest = createSimpleManifest( sv, creatorName, parentClassName );
+		modelManifest.additionalJoints = extraJoints;
+		for (ModelManifest.Joint rootJoint : getRootJoints(modelJoints) ) {
+			rootJoint.visibility = Visibility.COMPLETELY_HIDDEN;
+			modelManifest.rootJoints.add(rootJoint.name);
+		}
+
+		BufferedImage thumbnail = AdaptiveRecenteringThumbnailMaker.getInstance( 160, 120 ).createThumbnail( sv );
 		GalleryModelIo modelIo = new GalleryModelIo( sv, thumbnail, modelManifest );
 		try {
 			modelIo.writeModel( StageIDE.getActiveInstance().getGalleryDirectory() );
@@ -134,7 +191,133 @@ public class ImportGalleryResourceComposite extends ValueCreatorInputDialogCoreC
 		}
 	}
 
-	private static ModelManifest createSimpleManifest( SkeletonVisual sv, String creatorName, String parentClassName ) {
+	private static List<Field> getJointIdFields(Class<?> resourceClass) {
+		Field[] fields = resourceClass.getDeclaredFields();
+		List<Field> jointFields = new ArrayList<Field>();
+		for (Field f : fields) {
+			if (JointId.class.isAssignableFrom(f.getType())) {
+				jointFields.add(f);
+			}
+		}
+		return jointFields;
+	}
+
+	private static void addMissingJoints(SkeletonVisual sv, List<ModelManifest.Joint> missingJoints) {
+		int index = 0;
+		while (!missingJoints.isEmpty()) {
+			ModelManifest.Joint currentJoint = missingJoints.get(index);
+			if (currentJoint.parent != null) {
+				Joint parentJoint = sv.skeleton.getValue().getJoint(currentJoint.parent);
+				if (parentJoint != null) {
+					Joint newJoint = new Joint();
+					newJoint.jointID.setValue(currentJoint.name);
+					newJoint.setParent(parentJoint);
+					missingJoints.remove(index);
+					System.out.println("   ADDED JOINT "+currentJoint.toString());
+				} else {
+					System.out.println("   NO JOINT FOUND ON SKELETON FOR "+currentJoint.parent+", SKIPPING IT");
+					index = (index + 1) % missingJoints.size();
+				}
+			}
+			else {
+				System.out.println("   NO PARENT JOINT FOR "+currentJoint.toString()+", CAN'T ADD IT");
+			}
+		}
+	}
+
+	private static void addJointsToList( Joint sgJoint, List<ModelManifest.Joint> jointList ) {
+		jointList.add(createJoint(sgJoint));
+		for (Component c : sgJoint.getComponents()) {
+			if (c instanceof Joint) {
+				addJointsToList((Joint)c, jointList);
+			}
+		}
+	}
+
+	private static List<ModelManifest.Joint> getModelJoints(SkeletonVisual sv) {
+		List<ModelManifest.Joint> joints = new ArrayList<>();
+		addJointsToList(sv.skeleton.getValue(), joints);
+		return joints;
+	}
+
+	private static List<ModelManifest.Joint> getRootJoints(List<ModelManifest.Joint> jointList) {
+		List<ModelManifest.Joint> rootJoints = new ArrayList<>();
+		for (ModelManifest.Joint joint : jointList) {
+			if (joint.parent == null) {
+				rootJoints.add(joint);
+			}
+		}
+		return rootJoints;
+	}
+
+	private static Visibility getJointVisibility( JointId jointId ) {
+		Field jointField = jointId.getPublicStaticFinalFld();
+		if( jointField.isAnnotationPresent( FieldTemplate.class ) ) {
+			FieldTemplate propertyFieldTemplate = jointField.getAnnotation( FieldTemplate.class );
+			return propertyFieldTemplate.visibility();
+		}
+		return Visibility.PRIME_TIME;
+	}
+
+	private static ModelManifest.Joint createJoint(JointId jointId) {
+		ModelManifest.Joint joint = new ModelManifest.Joint();
+		joint.name = jointId.toString();
+		joint.parent = jointId.getParent().toString();
+		joint.visibility = getJointVisibility(jointId);
+		return joint;
+	}
+
+	private static ModelManifest.Joint createJoint(Joint sgJoint) {
+		ModelManifest.Joint joint = new ModelManifest.Joint();
+		joint.name = sgJoint.jointID.getValue();
+		if (sgJoint.getParent() instanceof Joint) {
+			joint.parent = ((Joint)sgJoint.getParent()).jointID.getValue();
+		}
+		else {
+			joint.parent = null;
+		}
+		joint.visibility = null;
+		return joint;
+	}
+
+	private static List<ModelManifest.Joint> getBaseJoints(Class<? extends ModelResource> resourceClass) {
+		List<Field> baseJointFields = getJointIdFields(resourceClass);
+		List<ModelManifest.Joint> baseJoints = new ArrayList<>();
+		for (Field f : baseJointFields) {
+			try {
+				JointId id = (JointId) f.get(null);
+				baseJoints.add(createJoint(id));
+			} catch (IllegalAccessException iae) {
+				iae.printStackTrace();
+			}
+		}
+		return baseJoints;
+	}
+
+	private static List<ModelManifest.Joint> getExtraJoints(List<ModelManifest.Joint> modelJoints, List<ModelManifest.Joint> baseJoints, boolean setVisible) {
+		List<ModelManifest.Joint> extraJoints = new ArrayList<>();
+		for (ModelManifest.Joint joint : modelJoints) {
+			if (!baseJoints.contains(joint)) {
+				if (setVisible) {
+					joint.visibility = Visibility.PRIME_TIME;
+				}
+				extraJoints.add(joint);
+			}
+		}
+		return extraJoints;
+	}
+
+	private static List<ModelManifest.Joint> getMissingJoints(List<ModelManifest.Joint> modelJoints, List<ModelManifest.Joint> requiredJoints) {
+		List<ModelManifest.Joint> missingJoints = new ArrayList<>();
+		for (ModelManifest.Joint joint : requiredJoints) {
+			if (!modelJoints.contains(joint)) {
+				missingJoints.add(joint);
+			}
+		}
+		return missingJoints;
+	}
+
+	private static ModelManifest createSimpleManifest( SkeletonVisual sv, String creatorName, String parentClassName) {
 		String modelName = sv.getName();
 		AxisAlignedBox boundingBox = sv.getAxisAlignedMinimumBoundingBox();
 
