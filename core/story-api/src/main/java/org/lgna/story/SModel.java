5dc851bfd3f89da0a7e2075e7c4cc70e4752658a
Daniel Abeshouse <dabeshou@andrew.cmu.edu>
2019-01-18 12:04:11 -0500

    T346 Move say() and think() from JointedModel up to Model

--- core/story-api/src/main/java/org/lgna/story/SModel.java
+++ core/story-api/src/main/java/org/lgna/story/SModel.java
@@ -196,4 +196,35 @@ public abstract class SModel extends SMovableTurnable implements MutableRider, R
 		//todo: explain how to make things smaller
 		this.getImplementation().animateResizeDepth( factor.doubleValue(), IsVolumePreserved.getValue( details ), Duration.getValue( details ), AnimationStyle.getValue( details ).getInternal() );
 	}
+
+	@MethodTemplate( )
+	public void say( String text, Say.Detail... details ) {
+		LgnaIllegalArgumentException.checkArgumentIsNotNull( text, 0 );
+		double textScale = TextScale.getValue( details );
+		int textSize = (int)( 16 * textScale );
+		int textStyle = TextStyle.getValue( details ).getInternal();
+		this.getImplementation().say( text, Duration.getValue( details ), TextFont.getValue( details, null, textStyle, textSize ).getAsAWTFont(), TextColor.getValue( details, Color.BLACK ).getInternal(), BubbleFillColor.getValue( details, Color.WHITE ).getInternal(), BubbleOutlineColor.getValue( details, Color.WHITE ).getInternal(), BubblePosition.getValue( details ).getInternal() );
+	}
+
+	@MethodTemplate( )
+	public void think( String text, Think.Detail... details ) {
+		LgnaIllegalArgumentException.checkArgumentIsNotNull( text, 0 );
+		double textScale = TextScale.getValue( details );
+		int textSize = (int)( 16 * textScale );
+		int textStyle = TextStyle.getValue( details ).getInternal();
+		this.getImplementation().think( text, Duration.getValue( details ), TextFont.getValue( details, null, textStyle, textSize ).getAsAWTFont(), TextColor.getValue( details, Color.BLACK ).getInternal(), BubbleFillColor.getValue( details, Color.WHITE ).getInternal(), BubbleOutlineColor.getValue( details, Color.WHITE ).getInternal(), BubblePosition.getValue( details ).getInternal() );
+	}
+
+	//TODO: Get this to work
+	//	@MethodTemplate()
+	//	public void sayOutLoud( String text, org.alice.flite.VoiceType voice, SayOutLoud.Detail... details ) {
+	//		SpeechBubble bubble = null;
+	//		if (ShowSpeechBubble.getValue(details, true))
+	//		{
+	//			bubble = new SpeechBubble();
+	//			bubble.text.setValue(text);
+	//			initializeBubble(bubble, details);
+	//		}
+	//		this.getImplementation().sayText(text, voice, bubble);
+	//	}
 }
