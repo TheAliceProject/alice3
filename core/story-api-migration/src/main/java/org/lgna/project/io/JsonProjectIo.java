04ff70588ff8640556677378d2647d9765dd9fa4
Daniel Abeshouse <dabeshou@andrew.cmu.edu>
2020-03-13 10:45:26 -0400

    Update required tweedle library to 0.13

--- core/story-api-migration/src/main/java/org/lgna/project/io/JsonProjectIo.java
+++ core/story-api-migration/src/main/java/org/lgna/project/io/JsonProjectIo.java
@@ -239,7 +239,7 @@ public class JsonProjectIo extends DataSourceIo implements ProjectIo {
     private Manifest.ProjectIdentifier standardLibrary() {
       final Manifest.ProjectIdentifier libraryIdentifier = new Manifest.ProjectIdentifier();
       libraryIdentifier.type = Manifest.ProjectType.Library;
-      libraryIdentifier.version = "0.11";
+      libraryIdentifier.version = "0.13";
       libraryIdentifier.name = "SceneGraphLibrary";
       return libraryIdentifier;
     }
