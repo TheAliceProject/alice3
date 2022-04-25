06d69178484b20d344fab3002e1f236ae6dbc041
Daniel Abeshouse <dabeshouse@gmail.con>
2022-04-25 14:10:20 -0400

    T1254 Increment required library version so older players reject exports

--- core/story-api-migration/src/main/java/org/lgna/project/io/JsonProjectIo.java
+++ core/story-api-migration/src/main/java/org/lgna/project/io/JsonProjectIo.java
@@ -262,7 +262,7 @@ public class JsonProjectIo extends DataSourceIo implements ProjectIo {
     private Manifest.ProjectIdentifier standardLibrary() {
       final Manifest.ProjectIdentifier libraryIdentifier = new Manifest.ProjectIdentifier();
       libraryIdentifier.type = Manifest.ProjectType.Library;
-      libraryIdentifier.version = "0.14";
+      libraryIdentifier.version = "0.16";
       libraryIdentifier.name = "SceneGraphLibrary";
       return libraryIdentifier;
     }
