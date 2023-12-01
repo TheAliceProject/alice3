08db310d8c545f46e25eca892e68def31248a8e1
Daniel Abeshouse <dabeshouse@gmail.com>
2023-12-01 15:46:12 -0500

    Adjust migration to split camera orientation

--- core/story-api-migration/src/main/java/org/lgna/project/migration/ast/ReplaceCameraWithVR.java
+++ core/story-api-migration/src/main/java/org/lgna/project/migration/ast/ReplaceCameraWithVR.java
@@ -1,6 +1,7 @@
 package org.lgna.project.migration.ast;
 
 import edu.cmu.cs.dennisc.java.util.logging.Logger;
+import edu.cmu.cs.dennisc.math.Angle;
 import edu.cmu.cs.dennisc.math.AngleInRadians;
 import edu.cmu.cs.dennisc.math.EulerAngles;
 import edu.cmu.cs.dennisc.math.OrthogonalMatrix3x3;
@@ -13,6 +14,7 @@ import org.lgna.project.migration.AstMigration;
 import org.lgna.project.migration.MigrationManager;
 import org.lgna.project.virtualmachine.InstanceCreatingVirtualMachine;
 import org.lgna.story.*;
+import org.lgna.story.Orientation;
 
 import java.util.ArrayList;
 import java.util.function.BiFunction;
@@ -24,10 +26,12 @@ public class ReplaceCameraWithVR extends AstMigration {
 
   AbstractType<?, ?, ?> cameraType = JavaType.getInstance(SCamera.class);
   AbstractType<?, ?, ?> vrUserType = JavaType.getInstance(SVRUser.class);
+  InstanceCreatingVirtualMachine vm = new InstanceCreatingVirtualMachine();
   private final String sCamera = SCamera.class.getSimpleName();
   private final String getHeadset = "getHeadset";
   private final String setPositionRelativeToVehicle = "setPositionRelativeToVehicle";
   private final String setOrientationRelativeToVehicle = "setOrientationRelativeToVehicle";
+  private final AngleInRadians zero = new AngleInRadians(0);
 
   public ReplaceCameraWithVR() {
     super(ProjectVersion.getCurrentVersion());
@@ -102,46 +106,84 @@ public class ReplaceCameraWithVR extends AstMigration {
     Logger.outln(String.format("Changed from SCamera.%s to SVRUser.%s", cameraMethod.getName(), vrUserMethod.getName()));
   }
 
-  private void splitVrOrientationStatement(MethodInvocation invocation, MigrationManager manager) {
-    // Individual ExpressionStatement holding the invocation
-    Node stmt = invocation.getParent();
+  private void splitVrOrientationStatement(MethodInvocation setOrientationCall, MigrationManager manager) {
+    // Get the ExpressionStatement holding the setOrientationCall
+    Node stmt = setOrientationCall.getParent();
+    // Get the block containing the statement
     Node grandparent = stmt.getParent();
     if (grandparent instanceof BlockStatement) {
       BlockStatement block = (BlockStatement) grandparent;
-      Expression orientationExp = invocation.requiredArguments.get(0).expression.getValue();
+      Expression orientationExp = setOrientationCall.requiredArguments.get(0).expression.getValue();
       if (orientationExp instanceof InstanceCreation) {
-        InstanceCreatingVirtualMachine vm = new InstanceCreatingVirtualMachine();
         InstanceCreation creation = (InstanceCreation) orientationExp;
         final Object ori = vm.createInstance(creation);
         if (ori instanceof Orientation) {
           Orientation cameraOrientation = (Orientation) ori;
-          OrthogonalMatrix3x3 matrix = EmployeesOnly.getOrthogonalMatrix3x3(cameraOrientation);
-          EulerAngles angles = new EulerAngles(matrix);
 
-          EulerAngles newCamAngles = new EulerAngles(new AngleInRadians(0), angles.yaw, new AngleInRadians(0), angles.order);
-          UnitQuaternion newCamQuat = newCamAngles.createUnitQuaternion();
-          SimpleArgumentListProperty args = creation.requiredArguments;
-          args.get(0).expression.setValue(new DoubleLiteral(newCamQuat.x));
-          args.get(1).expression.setValue(new DoubleLiteral(newCamQuat.y));
-          args.get(2).expression.setValue(new DoubleLiteral(newCamQuat.z));
-          args.get(3).expression.setValue(new DoubleLiteral(newCamQuat.w));
+          UnitQuaternion vrUserOrientation = getVrUserOrientation(cameraOrientation);
+          replaceOrientationArgs(creation, vrUserOrientation);
 
-          EulerAngles headAngles = new EulerAngles(angles.pitch, new AngleInRadians(0), angles.roll, angles.order);
-          UnitQuaternion headQuat = headAngles.createUnitQuaternion();
+          UnitQuaternion headsetOrientation = getHeadsetOrientation(cameraOrientation);
+          ExpressionStatement setHeadsetOrientation =
+              setHeadsetOrientationStatement(setOrientationCall.expression.getValue(), headsetOrientation);
+          manager.addFinalization(() -> block.statements.add(setHeadsetOrientation));
 
-          AbstractMethod headsetMethod = vrUserType.findMethod(getHeadset);
-          Expression getHeadsetExpression = new MethodInvocation(invocation.expression.getValue(), headsetMethod);
-          AbstractMethod setOrientation = AstUtilities.lookupMethod(SVRHeadset.class, setOrientationRelativeToVehicle, Orientation.class, SetOrientationRelativeToVehicle.Detail[].class);
-
-          JavaConstructor constructor = JavaConstructor.getInstance(Orientation.class, Number.class, Number.class, Number.class, Number.class);
-          InstanceCreation headOrientation = AstUtilities.createInstanceCreation(constructor, new DoubleLiteral(headQuat.x), new DoubleLiteral(headQuat.y), new DoubleLiteral(headQuat.z), new DoubleLiteral(headQuat.w));
-
-          manager.addFinalization(() -> block.statements.add(AstUtilities.createMethodInvocationStatement(getHeadsetExpression, setOrientation, headOrientation)));
           Logger.outln("Moved orientation from SCamera to SVRUser headset");
         }
       }
     }
 
+  }
+
+  private UnitQuaternion getVrUserOrientation(Orientation cameraOrientation) {
+    OrthogonalMatrix3x3 matrix = EmployeesOnly.getOrthogonalMatrix3x3(cameraOrientation);
+    EulerAngles angles = new EulerAngles(matrix);
+
+    Angle flatPitch = new AngleInRadians(nearestPi(angles.pitch));
+    Angle flatRoll = new AngleInRadians(nearestPi(angles.roll));
+    EulerAngles vrUserAngles = new EulerAngles(flatPitch, angles.yaw, flatRoll, angles.order);
+    return vrUserAngles.createUnitQuaternion();
+  }
+
+  private double nearestPi(Angle angle) {
+    double radians = angle.getAsRadians();
+
+    int halfTurns = (int) (radians / Math.PI);
+    // Set to absolute lower bound
+    halfTurns = (radians < 0) ? halfTurns - 1 : halfTurns;
+
+    // Pick closest half turn, below or above
+    if (radians > (0.5 + halfTurns) * Math.PI) {
+      halfTurns++;
+    }
+    return Math.PI * halfTurns;
+  }
+
+  private static void replaceOrientationArgs(InstanceCreation creation, UnitQuaternion newOrientation) {
+    SimpleArgumentListProperty args = creation.requiredArguments;
+    args.get(0).expression.setValue(new DoubleLiteral(newOrientation.x));
+    args.get(1).expression.setValue(new DoubleLiteral(newOrientation.y));
+    args.get(2).expression.setValue(new DoubleLiteral(newOrientation.z));
+    args.get(3).expression.setValue(new DoubleLiteral(newOrientation.w));
+  }
+
+  private UnitQuaternion getHeadsetOrientation(Orientation cameraOrientation) {
+    EulerAngles angles = new EulerAngles(EmployeesOnly.getOrthogonalMatrix3x3(cameraOrientation));
+    Angle flatPitchOffset = new AngleInRadians(angles.pitch.getAsRadians() - nearestPi(angles.pitch));
+    Angle flatRollOffset = new AngleInRadians(angles.roll.getAsRadians() - nearestPi(angles.roll));
+    EulerAngles headsetAngles = new EulerAngles(flatPitchOffset, zero, flatRollOffset, angles.order);
+    return headsetAngles.createUnitQuaternion();
+  }
+
+  private ExpressionStatement setHeadsetOrientationStatement(Expression userExpression, UnitQuaternion headsetOrientation) {
+    AbstractMethod headsetMethod = vrUserType.findMethod(getHeadset);
+    Expression getHeadsetExpression = new MethodInvocation(userExpression, headsetMethod);
+    AbstractMethod setOrientation = AstUtilities.lookupMethod(SVRHeadset.class, setOrientationRelativeToVehicle, Orientation.class, SetOrientationRelativeToVehicle.Detail[].class);
+
+    JavaConstructor constructor = JavaConstructor.getInstance(Orientation.class, Number.class, Number.class, Number.class, Number.class);
+    InstanceCreation headOrientation = AstUtilities.createInstanceCreation(constructor, new DoubleLiteral(headsetOrientation.x), new DoubleLiteral(headsetOrientation.y), new DoubleLiteral(headsetOrientation.z), new DoubleLiteral(headsetOrientation.w));
+
+    return AstUtilities.createMethodInvocationStatement(getHeadsetExpression, setOrientation, headOrientation);
   }
 
   private void splitVrPositionStatement(MethodInvocation invocation, MigrationManager manager) {
@@ -153,7 +195,6 @@ public class ReplaceCameraWithVR extends AstMigration {
       BlockStatement block = (BlockStatement) grandparent;
       Expression positionExp = invocation.requiredArguments.get(0).expression.getValue();
       if (positionExp instanceof InstanceCreation) {
-        InstanceCreatingVirtualMachine vm = new InstanceCreatingVirtualMachine();
         InstanceCreation creation = (InstanceCreation) positionExp;
         final Object pos = vm.createInstance(creation);
         Runnable result;
@@ -161,14 +202,15 @@ public class ReplaceCameraWithVR extends AstMigration {
           Position cameraPosition = (Position) pos;
 
           SimpleArgument arg = creation.requiredArguments.get(1);
-          arg.expression.setValue(new DoubleLiteral(cameraPosition.getUp() - 1.56));
+          double defaultHeight = 1.56;
+          arg.expression.setValue(new DoubleLiteral(cameraPosition.getUp() - defaultHeight));
 
           AbstractMethod headsetMethod = vrUserType.findMethod(getHeadset);
           Expression getHeadsetExpression = new MethodInvocation(invocation.expression.getValue(), headsetMethod);
           AbstractMethod setPosition = AstUtilities.lookupMethod(SVRHeadset.class, setPositionRelativeToVehicle, Position.class, SetPositionRelativeToVehicle.Detail[].class);
 
           JavaConstructor constructor = JavaConstructor.getInstance(Position.class, Number.class, Number.class, Number.class);
-          InstanceCreation headPosition = AstUtilities.createInstanceCreation(constructor, new DoubleLiteral(0.0), new DoubleLiteral(1.56), new DoubleLiteral(0.0));
+          InstanceCreation headPosition = AstUtilities.createInstanceCreation(constructor, new DoubleLiteral(0.0), new DoubleLiteral(defaultHeight), new DoubleLiteral(0.0));
 
           result = () -> block.statements.add(AstUtilities.createMethodInvocationStatement(getHeadsetExpression, setPosition, headPosition));
           manager.addFinalization(result);
