d144ebf8602e60eec59bf0b10caaf8051186908e
Daniel Abeshouse <dabeshouse@gmail.com>
2024-08-29 12:29:04 -0400

    Add launcher class to NetBeans projects to initialize JavaFX

--- netbeans/src/main/java/org/alice/netbeans/project/ProjectCodeGenerator.java
+++ netbeans/src/main/java/org/alice/netbeans/project/ProjectCodeGenerator.java
@@ -168,6 +168,10 @@ public class ProjectCodeGenerator {
       createWorkUnit++;
     }
 
+    FileObject fileObject = generateLauncher(javaSrcDirectory);
+    filesToOpen.add(fileObject);
+    progress(progressHandle, "create: ", fileObject, createWorkUnit);
+
     if (progressHandle != null) {
       progressHandle.switchToDeterminate(fileObjectsToFormat.size());
     }
@@ -204,4 +208,33 @@ public class ProjectCodeGenerator {
     }
     return filesToOpen;
   }
+
+  private static FileObject generateLauncher(File javaSrcDirectory) {
+    String fileName = "AliceJavaFXLauncher.java";
+    File file = new File(javaSrcDirectory, fileName);
+    TextFileUtilities.write(file, LAUNCHER_FILE);
+    return FileUtil.toFileObject(file);
+  }
+
+  private static final String LAUNCHER_FILE =
+"""
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+// If this project will not build and run make sure it is using
+// a JDK that includes JavaFX, such as Bellsoft's Liberica JDK.
+public class AliceJavaFXLauncher extends Application {
+    private static String[] startingArgs;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Thread thread = new Thread(() -> Program.main(startingArgs));
+        thread.start();
+    }
+
+    public static void main(final String[] args) {
+        startingArgs = args;
+        launch(args);
+    }
+}""";
 }
