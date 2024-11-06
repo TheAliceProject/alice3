dccb70944974ee97e49519fc2eb0e388136f3cdf
Daniel Abeshouse <dabeshouse@gmail.com>
2024-11-06 13:19:32 -0500

    Update ray viewport perspective logic to correct direction bug.

--- core/util/src/main/java/org/alice/math/immutable/Ray.java
+++ core/util/src/main/java/org/alice/math/immutable/Ray.java
@@ -3,6 +3,11 @@ package org.alice.math.immutable;
 public record Ray(Point3 origin, Vector3 direction) {
   public static final Ray NaN = new Ray(Point3.NaN, Vector3.NaN);
 
+  public static Ray fromAtoB(Point3 start, Point3 end) {
+    Vector3 direction = end.minus(start).normalized();
+    return new Ray(start, direction);
+  }
+
   public boolean isNaN() {
     return origin.isNaN() || direction.isNaN();
   }
