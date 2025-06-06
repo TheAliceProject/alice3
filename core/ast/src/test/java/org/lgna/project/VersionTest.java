26382e7fcd8a096c00e1575f832df32623856fef
Dmitry Portnoy <dportnoy@medievaltech.com>
2025-06-06 16:11:02 -0400

    Rename and split up some tests for Version

--- core/ast/src/test/java/org/lgna/project/VersionTest.java
+++ core/ast/src/test/java/org/lgna/project/VersionTest.java
@@ -9,13 +9,22 @@ import static org.junit.Assert.*;
 public class VersionTest {
 
     @Test
-    public void testIsValid() {
+    public void willReadValidVersionFromValidString() {
         assertTrue(new Version("3.14.0.4-alpha.2+build.local").isValid());
+    }
+
+    @Test
+    public void willReadInvalidVersionFromInvalidString() {
         assertFalse(new Version("3vhs14.0.4-alpha.2+build.local").isValid());
     }
 
     @Test
-    public void testToString() {
+    public void willReadInvalidVersionFromInvalidString2() {
+        assertFalse(new Version("3.14.0.4alpha.2+build.local").isValid());
+    }
+
+    @Test
+    public void willCreateVersionFromStringAndGetBackMatchingString() {
         String versionString = "3.14.0.4-alpha.2+build.local";
         Version v = new Version(versionString);
 
@@ -23,11 +32,11 @@ public class VersionTest {
     }
 
     @Test
-    public void testCurrentVersion() {
-        String currewntVersionString = TextFileUtilities.read(Version.class.getResourceAsStream("Version.txt")).trim();
-        Version v = new Version(currewntVersionString);
+    public void willReadCurrentVersionFromFileAndGetCorrectAndValidVersion() {
+        String currentVersionString = TextFileUtilities.read(Version.class.getResourceAsStream("Version.txt")).trim();
+        Version v = new Version(currentVersionString);
 
         assertTrue(v.isValid());
-        assertEquals(v.toString(), currewntVersionString);
+        assertEquals(v.toString(), currentVersionString);
     }
 }
