dba74bfc898442b2766386bd19ade67b814234a5
Dmitry Portnoy <dportnoy@medievaltech.com>
2026-01-15 16:46:39 -0500

    Add a checkstyle error to test merging

--- core/ide/src/main/java/edu/wustl/lookingglass/utilities/memory/HeapWatchDog.java
+++ core/ide/src/main/java/edu/wustl/lookingglass/utilities/memory/HeapWatchDog.java
@@ -52,6 +52,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * This shouldn't be necessary. But Looking Glass and Alice are not well behaved Java
